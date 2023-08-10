c094d0490de6ce70a29f55d91374d49d17351aa0
Oleh Onyshchenko <seesmwork@gmail.com>
2023-08-10 21:10:32 +0300

    delete 1 file and update 1 file

--- app/modern-js-from-beginning-traversy-media/page.tsx
+++ app/modern-js-from-beginning-traversy-media/page.tsx
@@ -1,17 +1,18 @@
 import React from "react";
+import Gallery from "./components/Gallery";
 
 const MainPage = () => {
+  const PagesData = [
+    {
+      name: "Console",
+      link: "console",
+    },
+  ];
+
   return (
-    <section className="bg-slate-900 text-slate-50 min-h-screen">
-      <div className="layout p-4 flex flex-col">
-        <p className="text-slate-200">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur
-          voluptatem, unde commodi fuga nesciunt eaque repellendus dignissimos,
-          reprehenderit sequi iure quibusdam dolor, nemo suscipit? Rerum
-          expedita at reprehenderit ullam voluptatem.
-        </p>
-      </div>
-    </section>
+    <>
+      <Gallery Pages={PagesData} pageName="Modern JavaScript from Beginning" />
+    </>
   );
 };
 
