478f8c6e23de30faf77124c888edfd6efa2ed427
Oleh Onyshchenko <seesmwork@gmail.com>
2023-08-24 12:09:25 +0000

    🌿: Thursday, August 24, 2023 at 3:09:25 PM Eastern European Summer Time

--- app/shadcn-play-around/page.tsx
+++ app/shadcn-play-around/page.tsx
@@ -226,16 +226,6 @@ export default function Home() {
     <main className="flex min-h-screen flex-col p-4 gap-4 max-w-6xl mx-auto">
       <ThemeToggle />
 
-      <Alert>
-        <Mail className="w-4 h-4" />
-        <AlertTitle>Alert</AlertTitle>
-        <AlertDescription>
-          This is a default alert. It can be dismissed. Please pay attention to
-          it. Use it whenever you feel like. Usage is not limited to just places
-          where you need it, but also to the places where you don't.
-        </AlertDescription>
-      </Alert>
-
       <Accordion type="single" collapsible>
         <AccordionItem value="1">
           <AccordionTrigger>Do we have a main office?</AccordionTrigger>
