4e80654f3f73021aa4f2a49ecd197d690f7de584
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-07-31 14:49:29 +0300

    feat: checkpoint ✝️💖 HALLELUJAH THANK YOU JESUS CHRIST our HOLY LORD GOD ALMIGHTY KING OF KINGS AND LORD OF LORDS

--- 31-07-2024 trying a BSB database, JESUS please help/a.py
+++ 31-07-2024 trying a BSB database, JESUS please help/a.py
@@ -1,24 +1,65 @@
 import os
 import json
+from pydantic import BaseModel
 
 
-root: str = os.path.dirname(os.path.abspath(__file__))
-Bible_files = os.path.join(root, "BSB")
-Bible_Books = os.path.join(root, "Bible_Books.yml")
-Bible_Books_concordance_file = os.path.join(root, "Bible_Books_concordance.json")
+Root: str = os.path.dirname(os.path.abspath(__file__))
+BIBLE_Text_Files = os.path.join(Root, "BSB")
+BIBLE_Books_File = os.path.join(Root, "Bible_Books.yml")
+BIBLE_Books_Numbered_File = os.path.join(Root, "Bible_Books.json")
 
 
-def form_Bible_Books_concordance():
-    Bible_Book_names = {}
-    with open(Bible_Books, "r", encoding="utf-8") as f:
+def Load_BIBLE_Books():
+    BIBLE_Books = {}
+    with open(BIBLE_Books_File, "r", encoding="utf-8") as f:
         lines = f.read().splitlines()
         for counter, line in enumerate(lines, start=1):
-            Bible_Book_names[counter] = line
-    return Bible_Book_names, len(Bible_Book_names.keys()) == 66
+            BIBLE_Books[counter] = line
+    return BIBLE_Books, len(BIBLE_Books.keys()) == 66
 
 
-Bible_Book_names, success = form_Bible_Books_concordance()
-if success:
-    with open(Bible_Books_concordance_file, "w", encoding="utf-8") as f:
-        json.dump(Bible_Book_names, f, indent=2)
-    print(Bible_Book_names)
+def Make_BIBLE_Json():
+    BIBLE_Books, success = Load_BIBLE_Books()
+    if not success:
+        print("Failed to open the file")
+    with open(BIBLE_Books_Numbered_File, "w", encoding="utf-8") as f:
+        json.dump(BIBLE_Books, f, indent=2)
+
+
+BIBLE_Books, _ = Load_BIBLE_Books()
+
+
+class Verse(BaseModel):
+    ID: int = 1
+    text: str = ""
+
+
+class Chapter(BaseModel):
+    ID: int = 1
+    Verses: list[Verse] = []
+
+
+class Book(BaseModel):
+    ID: int = 1
+    Chapters: list[Chapter] = []
+
+    def get_name(self):
+        return BIBLE_Books[self.ID]
+
+
+class BIBLE(BaseModel):
+    Books: list[Book] = []
+    Books_Number: int = 0
+
+
+# open each file in BIBLE_Text_Files:
+for filename in os.listdir(BIBLE_Text_Files):
+    name, ext = os.path.splitext(filename)
+
+    def extract_name(name):
+        Book_Number, Chapter_Number = name.replace("BSB ", "").split(" ")
+        Book_Name = BIBLE_Books[int(Book_Number)]
+        return Book_Name, int(Chapter_Number)
+
+    Book_Name, Chapter_Number = extract_name(name)
+    print(Book_Name, Chapter_Number)
