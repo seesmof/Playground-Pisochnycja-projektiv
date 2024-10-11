b232fe26da58f65838f5ef77cac3a4c27b220944
Oleh Onyshchenko <seesmwork@gmail.com>
2024-10-11 09:58:43 +0300

    feat: move BIBLE Kulish to its own project

--- Biblija Kulisha/Kopyjuval'nyk fajliv USFM.py
+++
@@ -1,6 +0,0 @@
-import os
-from shutil import copy2
-teka=os.path.dirname(os.path.abspath(__file__))
-fajly=os.path.join(teka,"BKS")
-projekt=os.path.join("C:\\My Paratext 9 Projects\\BKS")
-for f in os.listdir(fajly): copy2(os.path.join(fajly,f),os.path.join(projekt,f))
