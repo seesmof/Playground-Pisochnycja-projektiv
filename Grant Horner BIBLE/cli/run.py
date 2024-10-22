23836b55d617c0bc4bb67b337a407da24106f0d8
Oleh Onyshchenko <seesmwork@gmail.com>
2024-10-22 21:14:52 +0300

    feat: load data files

--- Grant Horner BIBLE/cli/run.py
+++ Grant Horner BIBLE/cli/run.py
@@ -1,3 +1,6 @@
 import os 
 import json 
 this_folder=os.path.dirname(os.path.abspath(__file__))
+with open(os.path.join(this_folder,"chapters.json")) as f: chapters=json.load(f)
+with open(os.path.join(this_folder,"lists.json")) as f: lists=json.load(f)
+with open(os.path.join(this_folder,"names.json")) as f: names=json.load(f)
