72f8e038e2a1133d96a161ea92c56c6b4ea33d92
Oleh Onyshchenko <seesmwork@gmail.com>
2024-10-23 14:06:23 +0300

    refactor: remove unnecessary projects

--- 21-10-2024 Написання тесту з РПП/11.py
+++
@@ -1,7 +0,0 @@
-from tkinter import *
-w=Tk()
-w.bind("<Escape>",lambda _:w.destroy())
-b=Button(w,text="Слава ІСУСУ ХРИСТУ")
-b.pack(padx=7,pady=7)
-b.bind("<Double-Button-1>",lambda _:b.config(text="Навіки слава!"))
-w.mainloop()
