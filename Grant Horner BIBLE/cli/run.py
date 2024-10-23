03d84cdeb7f75b6f868113819550fa5f83da1a0d
Oleh Onyshchenko <seesmwork@gmail.com>
2024-10-23 07:49:29 +0300

    refactor: extract getting current list data into a function, simplify showing current reading list function: JESUS THANK YOU LORD GOD MOST HIGH

--- Grant Horner BIBLE/cli/run.py
+++ Grant Horner BIBLE/cli/run.py
@@ -5,11 +5,15 @@ with open(os.path.join(this_folder,"chapters.json")) as f: chapters=json.load(f)
 with open(os.path.join(this_folder,"lists.json")) as f: lists=json.load(f)
 with open(os.path.join(this_folder,"names.json")) as f: names=json.load(f)
 
-def move_to_next_reading_on_list(list_number:int):
+def get_list_data(list_number:int):
     list_data=lists[list_number]
     chapter=list_data['chapter']
     Book=list_data['Book']
     Books=list_data['Books']
+    return chapter,Book,Books
+
+def move_to_next_reading_on_list(list_number:int):
+    chapter,Book,Books=get_list_data(list_number)
 
     if chapter < chapters[str(Books[Book])]:
         lists[list_number]['chapter']+=1
@@ -18,7 +22,9 @@ def move_to_next_reading_on_list(list_number:int):
         lists[list_number]['Book']=Book+1 if Book<len(Books)-1 else 0
 
 def show_current_reading_for_list(list_number:int):
-    print(names[str(lists[list_number]['Books'][lists[list_number]['Book']])], lists[list_number]['chapter'])
+    chapter,Book,Books=get_list_data(list_number)
+
+    print(names[str(Books[Book])], chapter)
 
 def get_readings_for_next_day():
     for list_number in range(10):
