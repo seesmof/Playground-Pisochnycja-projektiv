62836310acaed065420504bfa04e9ae5db591b51
Oleh Onyshchenko <seesmwork@gmail.com>
2024-10-23 07:46:35 +0300

    refactor: make moving to next reading function more readable: JESUS THANK  YOU LORD GOD ALMIGHTY

--- Grant Horner BIBLE/cli/run.py
+++ Grant Horner BIBLE/cli/run.py
@@ -6,11 +6,16 @@ with open(os.path.join(this_folder,"lists.json")) as f: lists=json.load(f)
 with open(os.path.join(this_folder,"names.json")) as f: names=json.load(f)
 
 def move_to_next_reading_on_list(list_number:int):
-    if lists[list_number]['chapter'] < chapters[str(lists[list_number]['Books'][lists[list_number]['Book']])]:
+    list_data=lists[list_number]
+    chapter=list_data['chapter']
+    Book=list_data['Book']
+    Books=list_data['Books']
+
+    if chapter < chapters[str(Books[Book])]:
         lists[list_number]['chapter']+=1
     else: 
         lists[list_number]['chapter']=1
-        lists[list_number]['Book']=lists[list_number]['Book']+1 if lists[list_number]['Book']<len(lists[list_number]['Books'])-1 else 0
+        lists[list_number]['Book']=Book+1 if Book<len(Books)-1 else 0
 
 def show_current_reading_for_list(list_number:int):
     print(names[str(lists[list_number]['Books'][lists[list_number]['Book']])], lists[list_number]['chapter'])
@@ -21,5 +26,7 @@ def get_readings_for_next_day():
         show_current_reading_for_list(list_number)
 
 def get_readings_for_specified_day(day:int): ...
+
+get_readings_for_next_day()
 
 with open(os.path.join(this_folder,"lists.json"),'w') as f: json.dump(lists,f,indent=2)
