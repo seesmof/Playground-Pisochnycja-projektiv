546c94ea7a4ddfafd602c43c42676584e1bed1e3
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-04 13:24:12 +0200

    refactor: format with yapf: JESUS THANK YOU

--- Grant Horner BIBLE/cli/run.py
+++ Grant Horner BIBLE/cli/run.py
@@ -1,93 +1,110 @@
-import os 
-import json 
-this_folder=os.path.dirname(os.path.abspath(__file__))
-get_path=lambda name:os.path.join(this_folder,name)
-with open(get_path('chapters.json')) as f: chapters=json.load(f)
-with open(get_path('names.json')) as f: names=json.load(f)
-with open(get_path('abbreviations.json')) as f: abbreviations=json.load(f)
-with open(get_path('shorts.json')) as f: eBible_abbreviations=json.load(f)
-with open(get_path('lists.json')) as f: lists=json.load(f)
+import os
+import json
 
-def get_list_data(list_number:int):
-    list_data=lists[list_number]
-    chapter=list_data['chapter']
-    Book=list_data['Book']
-    Books=list_data['Books']
-    return chapter,Book,Books
+this_folder = os.path.dirname(os.path.abspath(__file__))
+get_path = lambda name: os.path.join(this_folder, name)
+with open(get_path('chapters.json')) as f:
+    chapters = json.load(f)
+with open(get_path('names.json')) as f:
+    names = json.load(f)
+with open(get_path('abbreviations.json')) as f:
+    abbreviations = json.load(f)
+with open(get_path('shorts.json')) as f:
+    eBible_abbreviations = json.load(f)
+with open(get_path('lists.json')) as f:
+    lists = json.load(f)
 
-def move_to_next_reading_on_list(list_number:int):
-    chapter,Book,Books=get_list_data(list_number)
+
+def get_list_data(list_number: int):
+    list_data = lists[list_number]
+    chapter = list_data['chapter']
+    Book = list_data['Book']
+    Books = list_data['Books']
+    return chapter, Book, Books
+
+
+def move_to_next_reading_on_list(list_number: int):
+    chapter, Book, Books = get_list_data(list_number)
 
     if chapter < chapters[str(Books[Book])]:
-        lists[list_number]['chapter']+=1
-    else: 
-        lists[list_number]['chapter']=1
-        lists[list_number]['Book']=Book+1 if Book<len(Books)-1 else 0
+        lists[list_number]['chapter'] += 1
+    else:
+        lists[list_number]['chapter'] = 1
+        lists[list_number]['Book'] = Book + 1 if Book < len(Books) - 1 else 0
 
-def show_current_reading_for_list(list_number:int):
-    chapter,Book,Books=get_list_data(list_number)
+
+def show_current_reading_for_list(list_number: int):
+    chapter, Book, Books = get_list_data(list_number)
 
     print(names[str(Books[Book])], chapter)
 
-def open_link(link:str):
+
+def open_link(link: str):
     os.startfile(link)
 
+
 class Resource:
-    BOLLS_LIFE='Bolls.Life'
-    BIBLE_GATEWAY='BibleGateway'
-    YOU_VERSION='YouVersion'
-    E_BIBLE='eBible'
+    BOLLS_LIFE = 'Bolls.Life'
+    BIBLE_GATEWAY = 'BibleGateway'
+    YOU_VERSION = 'YouVersion'
+    E_BIBLE = 'eBible'
+
 
 class Language:
-    UK='Ukrainian'
-    EN='English'
+    UK = 'Ukrainian'
+    EN = 'English'
 
-def open_chapter(
-    Book_number:int,
-    chapter:int,
-    resource=Resource.E_BIBLE,
-    lang=Language.UK
-):
-    if resource==Resource.BOLLS_LIFE:
-        base_link='https://bolls.life'
-        version='UKRK' if lang==Language.UK else 'KJV'
-        ready_link=f'{base_link}/{version}/{Book_number}/{chapter}/'
+
+def open_chapter(Book_number: int,
+                 chapter: int,
+                 resource=Resource.E_BIBLE,
+                 lang=Language.UK):
+    if resource == Resource.BOLLS_LIFE:
+        base_link = 'https://bolls.life'
+        version = 'UKRK' if lang == Language.UK else 'KJV'
+        ready_link = f'{base_link}/{version}/{Book_number}/{chapter}/'
         open_link(ready_link)
-    elif resource==Resource.BIBLE_GATEWAY:
-        base_link='https://www.biblegateway.com/passage/?search'
-        passage=names[str(Book_number)]+str(chapter)
-        version='UKR' if lang==Language.UK else 'KJV'
-        ready_link=f'{base_link}={passage}&version={version}'
+    elif resource == Resource.BIBLE_GATEWAY:
+        base_link = 'https://www.biblegateway.com/passage/?search'
+        passage = names[str(Book_number)] + str(chapter)
+        version = 'UKR' if lang == Language.UK else 'KJV'
+        ready_link = f'{base_link}={passage}&version={version}'
         open_link(ready_link)
-    elif resource==Resource.YOU_VERSION:
-        base_link='https://www.bible.com/bible'
-        version='188' if lang==Language.UK else '1'
-        abbreviation=abbreviations[str(Book_number)]
-        ready_link=f'{base_link}/{version}/{abbreviation}.{chapter}'
+    elif resource == Resource.YOU_VERSION:
+        base_link = 'https://www.bible.com/bible'
+        version = '188' if lang == Language.UK else '1'
+        abbreviation = abbreviations[str(Book_number)]
+        ready_link = f'{base_link}/{version}/{abbreviation}.{chapter}'
         open_link(ready_link)
-    elif resource==Resource.E_BIBLE:
-        base_link="https://ebible.org/study/?w1=bible&t1=local%3A"
-        version='ukr1871' if lang==Language.UK else 'eng-kjv2006'
-        abbreviation=eBible_abbreviations[str(Book_number)]
-        ready_link=f'{base_link}{version}&v1={abbreviation}{chapter}'
+    elif resource == Resource.E_BIBLE:
+        base_link = "https://ebible.org/study/?w1=bible&t1=local%3A"
+        version = 'ukr1871' if lang == Language.UK else 'eng-kjv2006'
+        abbreviation = eBible_abbreviations[str(Book_number)]
+        ready_link = f'{base_link}{version}&v1={abbreviation}{chapter}'
         open_link(ready_link)
+
 
 def get_readings_for_next_day():
     for list_number in range(10):
         move_to_next_reading_on_list(list_number)
 
-        chapter,Book,Books=get_list_data(list_number)
-        open_chapter(Books[Book],chapter)
+        chapter, Book, Books = get_list_data(list_number)
+        open_chapter(Books[Book], chapter)
 
-        print(names[str(Books[Book])],chapter)
+        print(names[str(Books[Book])], chapter)
 
-def get_readings_for_specified_day(day:int): ...
 
-def skip_days(days:int):
+def get_readings_for_specified_day(day: int):
+    ...
+
+
+def skip_days(days: int):
     for day in range(days):
         for list_number in range(10):
             move_to_next_reading_on_list(list_number)
 
+
 get_readings_for_next_day()
 
-with open(get_path('lists.json'),'w') as f: json.dump(lists,f,indent=2)
+with open(get_path('lists.json'), 'w') as f:
+    json.dump(lists, f, indent=2)
