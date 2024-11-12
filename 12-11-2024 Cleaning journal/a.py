09a1b93836f34d8c2134020a3bc21a9bc79f1c62
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-12 14:15:42 +0200

    AMEN

--- 12-11-2024 Cleaning journal/a.py
+++ 12-11-2024 Cleaning journal/a.py
@@ -2,19 +2,10 @@ import glob
 import os
 from datetime import datetime
 
-
 root=os.path.dirname(os.path.abspath(__file__))
 target_folder=os.path.join(root,"entries")
-target_files=glob.glob(target_folder+"\\*.md")
-print(os.listdir(target_folder))
-ls=[]
-for file_path in target_files:
-    file_name=file_path.split('\\')[-1].replace('.md','')
-    day_header='### ' + file_name + '\n'
-    current_lines=[day_header]
-    with open(file_path,encoding='utf-8',mode='r') as f:
-        lines=f.readlines()
-    if not lines: 
-        print("Empty file",file_name)
-        os.remove(file_path)
-    current_lines+=lines
+target_files=os.listdir(target_folder)
+def get_formatted_date(date:str):
+    day,month,year=date.replace('.md','').split('-')
+    return year,month,day
+target_files.sort(key=get_formatted_date)
