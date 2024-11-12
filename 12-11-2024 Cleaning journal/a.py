4b307e32e09993c36f7a0d73e6abbd7c85de8902
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-12 14:00:18 +0200

    AMEN

---
+++ 12-11-2024 Cleaning journal/a.py
@@ -0,0 +1,12 @@
+import glob
+import os
+
+
+root=os.path.dirname(os.path.abspath(__file__))
+target_folder=os.path.join(root,"entries")
+target_files=glob.glob(target_folder+"\\*.md")
+for file_path in target_files:
+    with open(file_path,encoding='utf-8',mode='r') as f:
+        lines=f.readlines()
+    if not lines: 
+        print("Empty file",file_path.split('\\')[-1])
