73cd4035fc3ea3dac54a4177674ff6f20207c720
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-16 19:33:46 +0200

    AMEN

--- 16-11-2024 learning django/server/members/views.py
+++ 16-11-2024 learning django/server/members/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import Member
 
 
 def members(request):
-    template=loader.get_template('a.html')
-    return HttpResponse(template.render())
+    given_members=Member.objects.all().values()
+    template=loader.get_template('all_members.html')
+    context={
+        'given_members':given_members
+    }
+    return HttpResponse(template.render(context=context,request=request))
