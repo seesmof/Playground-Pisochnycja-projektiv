dc74a3ae7d44d380ca492dc73ab844b50654d4f1
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-16 19:39:01 +0200

    AMEN

--- 16-11-2024 learning django/server/members/urls.py
+++ 16-11-2024 learning django/server/members/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 
 urlpatterns=[
-    path('members/',views.members,name='members')
+    path('members/',views.members,name='members'),
+    path('members/details/<int:id>',views.details,name='details')
 ]
--- 16-11-2024 learning django/server/members/views.py
+++ 16-11-2024 learning django/server/members/views.py
@@ -10,3 +10,11 @@ def members(request):
         'given_members':given_members
     }
     return HttpResponse(template.render(context=context,request=request))
+
+def details(request,id):
+    member=Member.objects.get(id=id)
+    template=loader.get_template('details.html')
+    context={
+        'member':member
+    }
+    return HttpResponse(template.render(context=context,request=request))
