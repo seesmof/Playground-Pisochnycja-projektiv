ef67099efa903ea8d41fe53a2142d2b55c6c1c4a
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-16 19:17:44 +0200

    AMEN

--- 16-11-2024 learning django/server/members/views.py
+++ 16-11-2024 learning django/server/members/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
+from django.template import loader
 
 
 def members(request):
-    return HttpResponse('JESUS CHRIST IS LORD')
+    template=loader.get_template('a.html')
+    return HttpResponse(template.render())
