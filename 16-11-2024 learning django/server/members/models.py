685db39bfc45b310a822fc83446f9c3b9296614f
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-16 19:31:14 +0200

    AMEN

--- 16-11-2024 learning django/server/members/models.py
+++ 16-11-2024 learning django/server/members/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class Member(models.Model):
     first_name=models.CharField(max_length=255)
     last_name=models.CharField(max_length=255)
+    phone=models.IntegerField(null=True)
+    joined_date=models.DateField(null=True)
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
