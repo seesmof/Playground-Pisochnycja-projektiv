358ba8a2d77b9cfc860bfc8a6c97c64c790e486a
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-18 09:15:13 +0200

    AMEN

--- 16-11-2024 learning django/server/runner/settings.py
+++ 16-11-2024 learning django/server/runner/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=8mdx9ai$f1ja3d&u62v!*oqi8%k@ar@^i8s(zis^s)_!h4$ev'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
