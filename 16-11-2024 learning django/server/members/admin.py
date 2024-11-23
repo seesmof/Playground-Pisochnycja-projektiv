8218438cf399dda3d4bce64849ee0b47959965d3
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-23 13:01:36 +0200

    AMEN

--- 16-11-2024 learning django/server/members/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Member
-
-class MemberAdmin(admin.ModelAdmin):
-    list_display=('first_name','last_name','joined_date')
-
-admin.site.register(Member,MemberAdmin)
