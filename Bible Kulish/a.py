b975c465f48561359896b88e30d0cf80771da3fe
Oleh Onyshchenko <seesmwork@gmail.com>
2025-01-14 08:29:44 +0200

    Click to see changes

--- Bible Kulish/a.py
+++ Bible Kulish/a.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 root_folder_path=os.path.dirname(os.path.abspath(__file__))
 target_file_path=os.path.join(root_folder_path,'test.md')
@@ -14,6 +15,7 @@ replace all `\w'` and `'\w` with apostrophe symbols
 '''
 
 text=text.replace(' -',' —')
+text=re.sub(r'(\w)\'',r'ʼ',text)
 
 with open(target_file_path,encoding='utf-8',mode='w') as f:
     f.write(text)
