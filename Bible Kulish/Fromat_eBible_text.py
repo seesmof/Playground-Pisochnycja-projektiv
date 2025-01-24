6e1e1133cf2041b72776045dc8a5213091145a34
Oleh Onyshchenko <seesmwork@gmail.com>
2025-01-24 09:34:35 +0200

    Click to see changes

--- Bible Kulish/Fromat_eBible_text.py
+++ Bible Kulish/Fromat_eBible_text.py
@@ -4,12 +4,12 @@ import os
 import re
 
 root_folder_path=os.path.dirname(os.path.abspath(__file__))
-web_folder_path=os.path.join(root_folder_path,'WEB')
+web_folder_path=os.path.join(root_folder_path,'WEBU_Strongs')
 
 for file_name in os.listdir(web_folder_path):
     file_path=os.path.join(web_folder_path,file_name)
     with open(file_path,encoding='utf-8',mode='r') as f:
         lines=f.readlines()
-    lines=[re.sub(r'\|strong=\"[GH]\d{4}\"\\(\+?)w\*','',l).replace('\w ','').replace('\+w ','').strip() for l in lines]
+    lines=[l.strip() for l in lines]
     with open(file_path,encoding='utf-8',mode='w') as f:
         f.write('\n'.join(lines))
