2056c09eb55a8af8a4a5da07c76b3d55ef286a12
Oleh Onyshchenko <seesmwork@gmail.com>
2025-01-28 11:07:05 +0200

    Click to see changes

--- Bible Kulish/Markup_Text.py
+++ Bible Kulish/Markup_Text.py
@@ -4,7 +4,7 @@ import re
 
 root_folder_path=os.path.dirname(os.path.abspath(__file__))
 revision_folder_path=os.path.join(root_folder_path,'Revision')
-target_file_path=os.path.join(revision_folder_path,'37HAG.USFM')
+target_file_path=os.path.join(revision_folder_path,'34NAM.USFM')
 
 def mark_text(
     given_text:str,
