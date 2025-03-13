d85c0ae4dd79d685e182ae4558232962d91514e3
Oleh Onyshchenko <seesmwork@gmail.com>
2025-03-13 20:56:12 +0200

    Click to see changes

--- 20250313204003 Testing fuzzy search python/a.py
+++ 20250313204003 Testing fuzzy search python/a.py
@@ -7,7 +7,10 @@ res=difflib.SequenceMatcher(None,a,b).ratio()
 print(res)
 
 a="Так бо полюбив Бог сьвіт, що Сина свого єдинородного дав, щоб кожен, віруючий в Него, не погиб, а мав життє вічнє. Йоана 3:16"
+a="не погиб, а мав життє вічнє."
 b="не загинув але мав життя вічне"
+
+# split the original string into the substrings of the same length as input and do search this way
 
 res=difflib.SequenceMatcher(None,a,b).ratio()
 print(res)
