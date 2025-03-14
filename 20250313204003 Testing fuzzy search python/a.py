51f83c19b1cb97e39633d09ba366560d8cbae364
Oleh Onyshchenko <seesmwork@gmail.com>
2025-03-14 08:41:59 +0200

    Click to see changes

--- 20250313204003 Testing fuzzy search python/a.py
+++ 20250313204003 Testing fuzzy search python/a.py
@@ -4,7 +4,6 @@ a="Всяк бо, хто призове імя Господнє, спасеть 
 b="Бо всяк, хто призове ім'я Господнє, спасеться. Римляни 10:13"
 
 res=difflib.SequenceMatcher(None,a,b).ratio()
-print(res)
 
 a="Так бо полюбив Бог сьвіт, що Сина свого єдинородного дав, щоб кожен, віруючий в Него, не погиб, а мав життє вічнє. Йоана 3:16"
 a="не погиб, а мав життє вічнє."
@@ -12,5 +11,15 @@ b="не загинув але мав життя вічне"
 
 # split the original string into the substrings of the same length as input and do search this way
 
+def calcualte_similarity(source: str, given: str) -> float:
+    source_words: list[str] = source.split()
+    given_words: list[str] = given.split()
+    scores: list[float] = list()
+
+    for i, word in enumerate(source_words):
+        given_word 
+        score: float = difflib.SequenceMatcher(None, word, )
+
+
 res=difflib.SequenceMatcher(None,a,b).ratio()
-print(res)
+print(calcualte_similarity(a,b))
