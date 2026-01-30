5a71a23ee7a1e66bb8d5b117b36ee319d32ea7fc
Oleg Onyshchenko <seesmwork@gmail.com>
2026-01-30 20:56:10 +0200

    feat: provide a cli

--- 2026-01-30-todos-app/main.py
+++ 2026-01-30-todos-app/main.py
@@ -13,36 +13,25 @@ class Todo:
     status: Status = Status.PENDING
 
 
-root = tk.Tk()
-root.title("Todos App")
-root.geometry("600x400")
-
 todos = [
     Todo(name="Finish this app", status=Status.PENDING),
     Todo(name="Contact a teacher about diploma", status=Status.DONE),
 ]
 
 
-def format_todo(todo_item: Todo):
-    return f"{'✅' if todo_item.status == Status.DONE else '❌'} {todo_item.name}"
+def format_status(status):
+    return "✅" if status == Status.DONE else "❌"
 
 
-def submit_todo():
-    todo_text = input_box.get()
-    todo_item = Todo(name=todo_text)
-    todos.append(format_todo(todo_item))
+def show_todos():
+    for item in todos:
+        print(f"{format_status(item.status)}. {item.name}")
 
 
-input_box = tk.Entry(root)
-input_box.pack(padx=3, pady=3, fill="x")
-
-submit_button = tk.Button(root, text="Submit", command=submit_todo)
-submit_button.pack(padx=3)
-
-listbox = tk.Listbox(root)
-listbox.pack(padx=3, pady=3, fill="both", expand=True)
-for item in todos:
-    listbox.insert(tk.END, format_todo(item))
-
-if __name__ == "__main__":
-    root.mainloop()
+while True:
+    print()
+    todo_text = input("Please enter todo's text: ")
+    new_todo = Todo(name=todo_text)
+    todos.append(new_todo)
+    show_todos()
+    print()
