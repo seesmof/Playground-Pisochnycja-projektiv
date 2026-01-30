398bb4a5ed58dc1fc9a789c97fd0185b70a59022
Oleg Onyshchenko <seesmwork@gmail.com>
2026-01-30 21:39:40 +0200

    feat: can add new todos now

--- 2026-01-30-todos-app/todos/app/page.tsx
+++ 2026-01-30-todos-app/todos/app/page.tsx
@@ -8,16 +8,38 @@ interface Todo {
 }
 
 export default function Home() {
+  const [todos, setTodos] = useState<Todo[]>([
+    { text: "Finish a task", status: "Pending" },
+    { text: "Contact a professor", status: "Done" },
+  ]);
   const [text, setText] = useState("");
 
+  const addNewTodo = () => {
+    if (text.trim() === "") return;
+
+    const newTodo: Todo = { text: text, status: "Pending" };
+
+    setTodos((prevTodos) => [...prevTodos, newTodo]);
+    setText("");
+  };
+
   return (
-    <div className="p-3 w-full flex gap-3">
-      <input
-        className="input flex-1"
-        value={text}
-        onChange={(event) => setText(event.target.value)}
-      />
-      <button className="btn">Submit</button>
-    </div>
+    <>
+      <div className="p-3 w-full flex gap-3">
+        <input
+          className="input flex-1"
+          value={text}
+          onChange={(event) => setText(event.target.value)}
+        />
+        <button className="btn" onClick={addNewTodo}>
+          Submit
+        </button>
+      </div>
+      <ul className="p-3 flex flex-col list-disc list-inside">
+        {todos.map((item) => (
+          <li key={item.text}>{item.text}</li>
+        ))}
+      </ul>
+    </>
   );
 }
