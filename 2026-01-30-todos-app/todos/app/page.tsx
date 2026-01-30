2b6f80126dd6129cd2b016aa6d0f22414c326d77
Oleg Onyshchenko <seesmwork@gmail.com>
2026-01-30 21:07:10 +0200

    style: add an interface

--- 2026-01-30-todos-app/todos/app/page.tsx
+++ 2026-01-30-todos-app/todos/app/page.tsx
@@ -2,17 +2,22 @@
 
 import { useState } from "react";
 
+interface Todo {
+  text: string;
+  status: "Pending" | "Done";
+}
+
 export default function Home() {
   const [text, setText] = useState("");
 
   return (
-    <>
+    <div className="p-3 w-full flex gap-3">
       <input
-        className="input"
+        className="input flex-1"
         value={text}
         onChange={(event) => setText(event.target.value)}
       />
-      <p>Current text: {text}</p>
-    </>
+      <button className="btn">Submit</button>
+    </div>
   );
 }
