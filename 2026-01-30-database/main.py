7b14fe24131fb0d314d66014ffeed6ac9490b3cc
Oleg Onyshchenko <seesmwork@gmail.com>
2026-01-30 15:09:35 +0200

    feat: writing data to a csv

--- 2026-01-30-database/main.py
+++ 2026-01-30-database/main.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+import os
 from nicegui import ui
 
 
@@ -15,13 +17,31 @@ class Time:
 
 class Person:
     EUGENE = "Eugene Castro"
-    CECILIA = "Cecilia Morrison"
-    EMILY = "Emily Alexander"
     WALTER = "Walter Ball"
-    VICENT = "Vincent Walters"
+    VINCENT = "Vincent Walters"
 
 
+@dataclass
 class Entry:
-    vehicle: Car
     time: Time
+    vehicle: Car
     person: Person
+
+
+data = [
+    Entry(time=Time.ONE, vehicle=Car.BOLT, person=Person.WALTER),
+    Entry(time=Time.TWO, vehicle=Car.BOLT, person=Person.EUGENE),
+    Entry(time=Time.ONE, vehicle=Car.FABIA, person=Person.EUGENE),
+    Entry(time=Time.ONE, vehicle=Car.BOLT, person=Person.VINCENT),
+    Entry(time=Time.TWO, vehicle=Car.LOGAN, person=Person.EUGENE),
+    Entry(time=Time.THREE, vehicle=Car.FABIA, person=Person.WALTER),
+    Entry(time=Time.ONE, vehicle=Car.BOLT, person=Person.VINCENT),
+]
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+file_name = "data.csv"
+file_path = os.path.join(current_dir, file_name)
+with open(file_path, encoding="utf-8", mode="w") as f:
+    f.write("time,vehicle,person\n")
+    for entry in data:
+        f.write(f"{entry.time},{entry.vehicle},{entry.person}\n")
