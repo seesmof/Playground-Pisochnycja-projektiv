6cfb3d0f1f4ee8a0d9932311b2572cc62163a180
Oleg Onyshchenko <seesmwork@gmail.com>
2026-01-30 20:49:05 +0200

    style: add tkinter gui

--- 2026-01-30-todos-app/main.py
+++ 2026-01-30-todos-app/main.py
@@ -10,7 +10,7 @@ class Status:
 @dataclass
 class Todo:
     name: str
-    status: Status
+    status: Status = Status.PENDING
 
 
 root = tk.Tk()
@@ -22,12 +22,27 @@ todos = [
     Todo(name="Contact a teacher about diploma", status=Status.DONE),
 ]
 
+
+def format_todo(todo_item: Todo):
+    return f"{'✅' if todo_item.status == Status.DONE else '❌'} {todo_item.name}"
+
+
+def submit_todo():
+    todo_text = input_box.get()
+    todo_item = Todo(name=todo_text)
+    todos.append(format_todo(todo_item))
+
+
+input_box = tk.Entry(root)
+input_box.pack(padx=3, pady=3, fill="x")
+
+submit_button = tk.Button(root, text="Submit", command=submit_todo)
+submit_button.pack(padx=3)
+
 listbox = tk.Listbox(root)
 listbox.pack(padx=3, pady=3, fill="both", expand=True)
 for item in todos:
-    listbox.insert(
-        tk.END, f"{'✅' if item.status == Status.DONE else '❌'} {item.name}"
-    )
+    listbox.insert(tk.END, format_todo(item))
 
 if __name__ == "__main__":
     root.mainloop()
