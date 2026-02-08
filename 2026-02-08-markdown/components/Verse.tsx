9fc9c7b07f9fcd494cf00c1d9d6bb6b4ea93f187
Oleg Onyshchenko <seesmwork@gmail.com>
2026-02-08 17:58:36 +0200

    feat: add a favicon, remove a Markdown page, add a link to Verse component

--- 2026-02-08-markdown/components/Verse.tsx
+++ 2026-02-08-markdown/components/Verse.tsx
@@ -1,14 +1,27 @@
+import Link from "next/link";
+
 export interface VerseType {
   text: string;
   reference: string;
 }
 
 export const Verse: React.FC<VerseType> = ({ text, reference }) => {
+  const url = `https://www.biblegateway.com/passage/?search=${reference}&version=NASB,UKR`;
   return (
-    <blockquote className="border-l-2 pl-4 border-sky-700">
+    <blockquote
+      className="border-l-2 pl-4 border-sky-700 cursor-pointer"
+      onClick={() => navigator.clipboard.writeText(text)}
+    >
       {text}{" "}
       <cite>
-        (<em>{reference}</em>)
+        (
+        <Link
+          href={url}
+          className="italic hover:underline underline-offset-4 decoration-sky-700"
+        >
+          {reference}
+        </Link>
+        )
       </cite>
     </blockquote>
   );
