8454c36e3b0d088a984cbbcf7b22fa1059f0fa2c
Oleg Onyshchenko <seesmwork@gmail.com>
2026-02-08 15:21:45 +0200

    feat: calculator make functional

--- 2026-02-04-calculator/app/page.tsx
+++ 2026-02-04-calculator/app/page.tsx
@@ -3,7 +3,13 @@
 import { useState } from "react";
 
 export default function Home() {
-  const [value, setValue] = useState("0");
+  const [value, setValue] = useState("");
+  const [result, setResult] = useState("");
+
+  const handleSolve = () => {
+    const result = eval(value);
+    setValue(result);
+  };
 
   return (
     <div className="bg-sky-50 flex items-center justify-center h-screen">
@@ -16,22 +22,102 @@ export default function Home() {
           value={value}
         />
         <div className="grid gap-2 grid-cols-4 grid-rows-4">
-          <button className="btn col-span-1">7</button>
-          <button className="btn col-span-1">8</button>
-          <button className="btn col-span-1">9</button>
-          <button className="btn col-span-1">/</button>
-          <button className="btn col-span-1">4</button>
-          <button className="btn col-span-1">5</button>
-          <button className="btn col-span-1">6</button>
-          <button className="btn col-span-1">*</button>
-          <button className="btn col-span-1">1</button>
-          <button className="btn col-span-1">2</button>
-          <button className="btn col-span-1">3</button>
-          <button className="btn col-span-1">-</button>
-          <button className="btn col-span-1">0</button>
-          <button className="btn col-span-1">,</button>
-          <button className="btn col-span-1">=</button>
-          <button className="btn col-span-1">+</button>
+          <button onClick={() => setValue("")} className="btn col-span-4">
+            Clean
+          </button>
+          <button
+            onClick={() => setValue(value + "7")}
+            className="btn col-span-1"
+          >
+            7
+          </button>
+          <button
+            onClick={() => setValue(value + "8")}
+            className="btn col-span-1"
+          >
+            8
+          </button>
+          <button
+            onClick={() => setValue(value + "9")}
+            className="btn col-span-1"
+          >
+            9
+          </button>
+          <button
+            onClick={() => setValue(value + "/")}
+            className="btn col-span-1"
+          >
+            /
+          </button>
+          <button
+            onClick={() => setValue(value + "4")}
+            className="btn col-span-1"
+          >
+            4
+          </button>
+          <button
+            onClick={() => setValue(value + "5")}
+            className="btn col-span-1"
+          >
+            5
+          </button>
+          <button
+            onClick={() => setValue(value + "6")}
+            className="btn col-span-1"
+          >
+            6
+          </button>
+          <button
+            onClick={() => setValue(value + "*")}
+            className="btn col-span-1"
+          >
+            *
+          </button>
+          <button
+            onClick={() => setValue(value + "1")}
+            className="btn col-span-1"
+          >
+            1
+          </button>
+          <button
+            onClick={() => setValue(value + "2")}
+            className="btn col-span-1"
+          >
+            2
+          </button>
+          <button
+            onClick={() => setValue(value + "3")}
+            className="btn col-span-1"
+          >
+            3
+          </button>
+          <button
+            onClick={() => setValue(value + "-")}
+            className="btn col-span-1"
+          >
+            -
+          </button>
+          <button
+            onClick={() => setValue(value + "0")}
+            className="btn col-span-1"
+          >
+            0
+          </button>
+          <button
+            onClick={() => setValue(value + ",")}
+            className="btn col-span-1"
+          >
+            ,
+          </button>
+          <button onClick={() => handleSolve()} className="btn col-span-1">
+            =
+          </button>
+          <button
+            onClick={() => setValue(value + "+")}
+            className="btn col-span-1"
+          >
+            +
+          </button>
         </div>
       </div>
     </div>
