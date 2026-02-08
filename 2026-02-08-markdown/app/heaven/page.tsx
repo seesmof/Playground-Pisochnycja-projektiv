e7fac2b3dc9004728eafba2a38155eb11f248831
Oleg Onyshchenko <seesmwork@gmail.com>
2026-02-08 18:03:54 +0200

    feat: add a page on Heaven, praise King Jesus!

--- 2026-02-08-markdown/app/heaven/page.tsx
+++ 2026-02-08-markdown/app/heaven/page.tsx
@@ -4,19 +4,19 @@ import { Verse, VerseType } from "@/components/Verse";
 import { useState } from "react";
 import Link from "next/link";
 
-export default function Love() {
+export default function Heaven() {
   const [verses, setVerses] = useState<VerseType[]>([
     {
-      text: "Love is patient and kind; love does not envy or boast; it is not arrogant or rude. It does not insist on its own way; it is not irritable or resentful; it does not rejoice at wrongdoing, but rejoices with the truth. Love bears all things, believes all things, hopes all things, endures all things. Love never ends. As for prophecies, they will pass away; as for tongues, they will cease; as for knowledge, it will pass away.",
-      reference: "1 Corinthians 13:4-8",
+      text: "In my Father's house are many rooms. If it were not so, would I have told you that I go to prepare a place for you?",
+      reference: "John 14:2",
     },
     {
-      text: "Let all that you do be done in love.",
-      reference: "1 Corinthians 16:14",
+      text: "He will wipe away every tear from their eyes, and death shall be no more, neither shall there be mourning, nor crying, nor pain anymore, for the former things have passed away.”",
+      reference: "Revelation 21:4",
     },
     {
-      text: "“For God so loved the world, that he gave his only Son, that whoever believes in him should not perish but have eternal life.",
-      reference: "John 3:16",
+      text: "But, as it is written, “What no eye has seen, nor ear heard, nor the heart of man imagined, what God has prepared for those who love him”—",
+      reference: "1 Corinthians 2:9",
     },
   ]);
 
@@ -31,13 +31,13 @@ export default function Love() {
             About
           </Link>
           <Link
-            className="underline underline-offset-4 decoration-sky-700"
+            className="hover:underline underline-offset-4 decoration-sky-700"
             href="/love/"
           >
             Love
           </Link>
           <Link
-            className="hover:underline underline-offset-4 decoration-sky-700"
+            className="underline underline-offset-4 decoration-sky-700"
             href="/heaven/"
           >
             Heaven
@@ -47,10 +47,10 @@ export default function Love() {
           <h2 className="mb-2 text-lg">
             Verses about{" "}
             <Link
-              href={"https://www.openbible.info/topics/love"}
+              href={"https://www.openbible.info/topics/heaven"}
               className="hover:underline underline-offset-4 decoration-sky-700"
             >
-              Love
+              Heaven
             </Link>
           </h2>
           <div className="flex flex-col gap-3">
