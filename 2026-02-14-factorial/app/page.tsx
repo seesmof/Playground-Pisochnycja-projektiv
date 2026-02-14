b2c478fea573fe48edbeeec02995560d54ff36ee
Oleg Onyshchenko <seesmwork@gmail.com>
2026-02-14 17:49:26 +0200

    feat -- factorial: make it have keyboard shortcuts, add a clear button

--- 2026-02-14-factorial/app/page.tsx
+++ 2026-02-14-factorial/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { FormEvent, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 
 const factorial = (num: number): number => {
   if (num < 0) return 0;
@@ -15,19 +15,41 @@ export default function Home() {
   const [input, setInput] = useState<string>("");
   const [result, setResult] = useState<number>(0);
 
-  const handleSubmit = (e: FormEvent) => {
-    e.preventDefault();
+  const calculateAndSetResults = () => {
     const num = Number.parseInt(input);
     if (!isNaN(num)) setResult(factorial(num));
   };
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    calculateAndSetResults();
+  };
+
+  const cleanseInputs = () => {
+    setInput("");
+    setResult(0);
+  };
+
+  const handleKeyDown = (event: KeyboardEvent) => {
+    if (event.key === "Enter" || event.key === "\n") calculateAndSetResults();
+    if (event.key === "Delete") cleanseInputs();
+  };
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  });
 
   return (
     <div className="bg-sky-50 min-h-screen p-3 flex items-center justify-center">
       <div className="bg-white rounded-md p-3 w-full max-w-xl">
         <form className="flex flex-row gap-2 w-full" onSubmit={handleSubmit}>
+          <button type="reset" className="btn" onClick={cleanseInputs}>
+            Clear
+          </button>
           <input
             type="number"
-            placeholder="A factorial number"
+            placeholder="A number to calculate factorial of..."
             className="input flex-1"
             value={input}
             onChange={(e) => setInput(e.target.value)}
@@ -41,6 +63,7 @@ export default function Home() {
           className="input mt-2 flex-1 w-full"
           readOnly
           value={result}
+          onClick={() => navigator.clipboard.writeText(result.toString())}
         />
       </div>
     </div>
