257f249077ea8b3b71f6ae56af76600bb4cfd99b
Oleg Onyshchenko <seesmwork@gmail.com>
2026-03-26 18:08:03 +0200

    feat: doing something

--- 2026-03-26-todo/app/page.tsx
+++ 2026-03-26-todo/app/page.tsx
@@ -7,8 +7,27 @@ interface Todo {
   message: string;
 }
 const Home = () => {
-  const [todos, setTodos] = useState<Todo[]>([]);
+  const [todos, setTodos] = useState<Todo[]>([
+    { isDone: false, message: "Learn Rust" },
+    { isDone: false, message: "Finish this task" },
+  ]);
 
-  return "Jesus is KING";
+  return (
+    <div className="bg-linear-to-br to-sky-50 min-h-screen p-3 flex items-center justify-center">
+      <div className="bg-white rounded-md shadow p-3">
+        {todos.map((todo) => (
+          <div key={todo.message} className="flex flex-row gap-3">
+            <input
+              type="checkbox"
+              checked={todo.isDone}
+              onChange={() => (todo.isDone = !todo.isDone)}
+              id={todo.message}
+            />
+            <label htmlFor={todo.message}>{todo.message}</label>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 export default Home;
