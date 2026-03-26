8384a4d200145967410059be56f5b054c282e114
Oleg Onyshchenko <seesmwork@gmail.com>
2026-03-26 15:47:16 +0200

    feat: add a simple app

---
+++ 2026-03-26-low-level/a.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main()
+{
+    printf("Jesus is LORD\n");
+
+    int y = 30;
+    printf("%d\n", y);
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("Jesus is King, %d\n", i);
+    }
+}
