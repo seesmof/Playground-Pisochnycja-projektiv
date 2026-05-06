bafa1cdab66e99a0a19259470448b9952de37aac
Oleg Onyshchenko <seesmwork@gmail.com>
2026-05-06 11:20:51 +0300

    feat: make a toggle button

--- 2026-05-06-test/app/page.tsx
+++ 2026-05-06-test/app/page.tsx
@@ -1,16 +1,20 @@
-interface ParagraphProps {
-  status: boolean;
-}
+"use client";
 
-function Paragraph({ status }: ParagraphProps) {
-  return <p style={{ color: status ? "slateblue" : "inherit" }}>Some text.</p>;
-}
+import { useState } from "react";
 
 export default function Page() {
+  const [status, setStatus] = useState<boolean>(false);
+
   return (
-    <div className="flex flex-col gap-3 p-3">
-      <Paragraph status={true} />
-      <Paragraph status={false} />
+    <div className="bg-sky-50 min-h-screen flex flex-col items-center justify-center">
+      <button
+        className={`rounded-md shadow p-3 cursor-pointer ${status ? "bg-amber-500" : "bg-white"}`}
+        onClick={() => {
+          setStatus((status) => !status);
+        }}
+      >
+        Click.
+      </button>
     </div>
   );
 }
