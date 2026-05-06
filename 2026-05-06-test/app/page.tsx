585c2dc83bc1d99dcb1ee9f98a67652a742e3060
Oleg Onyshchenko <seesmwork@gmail.com>
2026-05-06 10:59:10 +0300

    style: pass boolean prop to a component

--- 2026-05-06-test/app/page.tsx
+++ 2026-05-06-test/app/page.tsx
@@ -1,43 +1,16 @@
-"use client";
-
-import { useState } from "react";
-
-interface ProfileProps {
-  id: number;
-  name: string;
-  age: number;
+interface ParagraphProps {
+  status: boolean;
 }
 
-const Profile = ({ id, name, age }: ProfileProps) => {
-  return (
-    <div className="bg-white rounded-md shadow p-3 flex-col flex">
-      <h2 className="font-bold">{name}</h2>
-      <p className="italic">{age}</p>
-    </div>
-  );
-};
+function Paragraph({ status }: ParagraphProps) {
+  return <p style={{ color: status ? "slateblue" : "inherit" }}>Some text.</p>;
+}
 
 export default function Page() {
-  const [data, setData] = useState<ProfileProps[]>([
-    { id: 1, name: "Nell", age: 47 },
-    { id: 2, name: "Jorge", age: 60 },
-    { id: 3, name: "Ethan", age: 48 },
-    { id: 4, name: "Russell", age: 47 },
-    { id: 5, name: "Myrtle", age: 39 },
-  ]);
-
   return (
-    <div className="bg-sky-50 min-h-screen">
-      <div className="flex flex-col gap-3 p-3">
-        {data.map((object) => (
-          <Profile
-            id={object.id}
-            key={object.id}
-            name={object.name}
-            age={object.age}
-          />
-        ))}
-      </div>
+    <div className="flex flex-col gap-3 p-3">
+      <Paragraph status={true} />
+      <Paragraph status={false} />
     </div>
   );
 }
