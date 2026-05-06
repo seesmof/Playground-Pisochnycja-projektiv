a6ec419126a9e49aa483cf606000bcef8b480762
Oleg Onyshchenko <seesmwork@gmail.com>
2026-05-06 09:43:49 +0300

    refactor: toggle visibility of text

--- 2026-05-06-test/app/page.tsx
+++ 2026-05-06-test/app/page.tsx
@@ -6,13 +6,11 @@ export default function Page() {
   const [status, setStatus] = useState<boolean>(false);
 
   return (
-    <div className="min-h-screen bg-sky-50 flex items-center justify-center">
-      <div className="bg-white rounded-md shadow p-3 flex flex-col gap-3 w-32">
-        <button className="btn" onClick={() => setStatus(!status)}>
-          Toggle
-        </button>
-        <p hidden={status}>Test String</p>
-      </div>
+    <div className="flex flex-col p-3 gap-3">
+      <button className="btn" onClick={() => setStatus(!status)}>
+        Click
+      </button>
+      {status && <p>Some text.</p>}
     </div>
   );
 }
