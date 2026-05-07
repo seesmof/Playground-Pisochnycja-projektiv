3fa2fda9bc50344a362bef2e7cced93f649d8857
Oleg Onyshchenko <seesmwork@gmail.com>
2026-05-07 09:18:06 +0300

    feat: schedule the opening of AM

--- 2026-05-07-link-opener/main.py
+++ 2026-05-07-link-opener/main.py
@@ -1,4 +1,6 @@
 import os
+import time
+import schedule
 
 
 def open_url(url: str):
@@ -9,8 +11,16 @@ def copy_to_clipboard(text: str):
     os.system(f"echo {text.strip()}| clip")
 
 
-url: str = "https://us02web.zoom.us/j/86437456930"
-open_url(url=url)
+def open_am():
+    url: str = "https://us02web.zoom.us/j/86437456930"
+    open_url(url=url)
 
-code: str = "330748"
-copy_to_clipboard(code)
+    code: str = "330748"
+    copy_to_clipboard(code)
+
+
+schedule.every().thursday.at("14:55").do(open_am)
+
+while True:
+    schedule.run_pending()
+    time.sleep(5)
