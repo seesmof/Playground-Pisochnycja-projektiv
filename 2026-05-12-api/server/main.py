31c5e8447b320bccb562ab30eeade6a4b234eb34
Oleg Onyshchenko <seesmwork@gmail.com>
2026-05-12 16:32:09 +0300

    feat: the hello world in FastAPI, Jesus thank you

--- 2026-05-12-api/server/main.py
+++ 2026-05-12-api/server/main.py
@@ -1,41 +1,8 @@
-import os
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-def get_full_name(first_name: str, last_name: str) -> str:
-    return f"{first_name.title()} {last_name.title()}"
-
-
-first_name = "oleh"
-last_name = "onyshchenko"
-full_name = get_full_name(first_name=first_name, last_name=last_name)
-print(full_name)
-
-
-def get_name_with_age(name: str, age: int) -> str:
-    return f"{name} is {age} years old."
-
-
-name: str = "Clarence"
-age: int = 21
-name_with_age = get_name_with_age(name=name, age=age)
-print(name_with_age)
-
-
-def process_items(items: list[str]):
-    results: list[str] = list()
-    for index, item in enumerate(items, start=1):
-        formatted_item = f"{index}. {item}"
-        print(formatted_item)
-        results.append(formatted_item)
-    return results
-
-
-names = ["Susie", "Lizzie", "Julia"]
-listed_names: list[str] = process_items(names)
-
-current_dir = os.path.dirname(os.path.abspath(__file__))
-html_output_file_path: str = os.path.join(current_dir, "index.html")
-with open(html_output_file_path, encoding="utf-8", mode="w") as f:
-    f.write("<ul>")
-    f.writelines([f"<li>{line}</li>" for line in listed_names])
-    f.write("</ul>")
+@app.get("/")
+def root():
+    return {"message": "Jesus is LORD"}
