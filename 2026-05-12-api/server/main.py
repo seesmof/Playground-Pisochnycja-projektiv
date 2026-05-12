16dce883862d3a0c69c427e33baaa731fc616aa3
Oleg Onyshchenko <seesmwork@gmail.com>
2026-05-12 16:39:52 +0300

    feat: working with the items list

--- 2026-05-12-api/server/main.py
+++ 2026-05-12-api/server/main.py
@@ -2,7 +2,20 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+items = []
+
 
 @app.get("/")
 def root():
     return {"message": "Jesus is LORD"}
+
+
+@app.post("/items")
+def create_item(item: str):
+    items.append(item)
+    return items
+
+
+@app.get("/get_items")
+def get_items():
+    return items
