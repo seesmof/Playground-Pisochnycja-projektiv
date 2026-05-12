889ac6162fede302f71a1e50f98fe3a093873ded
Oleg Onyshchenko <seesmwork@gmail.com>
2026-05-12 15:56:07 +0300

    feat: going through the tutorial

--- 2026-05-12-api/server/main.py
+++ 2026-05-12-api/server/main.py
@@ -6,3 +6,13 @@ first_name = "oleh"
 last_name = "onyshchenko"
 full_name = get_full_name(first_name=first_name, last_name=last_name)
 print(full_name)
+
+
+def get_name_with_age(name: str, age: int) -> str:
+    return f"{name} is {age} years old."
+
+
+name: str = "Clarence"
+age: int = 21
+result = get_name_with_age(name=name, age=age)
+print(result)
