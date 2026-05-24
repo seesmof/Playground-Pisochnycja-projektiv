1bb52494d67f3e0cb3b6b82842ba3d676e55cb63
Oleg Onyshchenko <seesmwork@gmail.com>
2026-05-24 16:29:23 +0300

    feat: okay, fetching something

--- a.js
+++ a.js
@@ -1,18 +1,17 @@
-const fetchUser = () => {
-  return new Promise((resolve, reject) => {
-    let success = true;
-    if (success) resolve({ id: 1, name: "Oleh" });
-    else reject("Could not fetch user data.");
-  });
-};
-
-const getUserData = async () => {
+const getTodoItem = async () => {
   try {
-    const user = await fetchUser();
-    console.log(`Success: ${user.name}`);
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/todos/1",
+    );
+
+    if (!response.ok) throw new Error(`HTTP Error! Status ${response.status}`);
+
+    const data = await response.json();
+
+    console.log(`Here is our data: ${data.title}`);
   } catch (error) {
-    console.error(`Error: ${error}`);
+    console.error(`Something went wrong: ${error.message}`);
   }
 };
 
-getUserData();
+getTodoItem();
