cb6cbb0a18b31f0d9337c40af1e19b238af27541
Oleg Onyshchenko <seesmwork@gmail.com>
2026-05-24 16:44:48 +0300

    feat: all works well now, Jesus thank you

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,41 +10,45 @@ interface User {
 const API_URL = "https://jsonplaceholder.typicode.com/users";
 
 export default async function HomePage() {
+  let users: User[] | null = null;
+
   try {
     const response = await fetch(API_URL);
 
     if (!response.ok)
       throw new Error(`Failed to fetch users. Status: ${response.status}`);
 
-    const users: User[] = await response.json();
-
-    return (
-      <div className="max-w-4xl mx-auto p-3">
-        <h1 className="text-3xl font-bold mb-5 text-slate-800">Users List</h1>
-
-        <div className="grid gap-3 md:grid-cols-2">
-          {users.map((user, index) => (
-            <div
-              key={user.id}
-              className="p-3 border rounded-xl shadow-sm bg-white"
-            >
-              <h2 className="text-xl font-semibold text-blue-600">
-                {user.name}
-              </h2>
-              <p className="text-gray-600">{user.email}</p>
-              <p className="text-xs text-gray-400 mt-2">
-                Works at: {user.company.name}
-              </p>
-            </div>
-          ))}
-        </div>
-      </div>
-    );
+    users = await response.json();
   } catch (error) {
+    console.error(`Data fetching failed: ${error}`);
+  }
+
+  if (!users) {
     return (
       <div className="p-3 text-center text-red-500 font-semibold">
         Failed to load users. Please try again.
       </div>
     );
   }
+
+  return (
+    <div className="max-w-4xl mx-auto p-3">
+      <h1 className="text-3xl font-bold mb-5 text-slate-800">Users List</h1>
+
+      <div className="grid gap-3 md:grid-cols-2">
+        {users.map((user, index) => (
+          <div
+            key={user.id}
+            className="p-3 border rounded-xl shadow-sm bg-white"
+          >
+            <h2 className="text-xl font-semibold text-blue-600">{user.name}</h2>
+            <p className="text-gray-600">{user.email}</p>
+            <p className="text-xs text-gray-400 mt-2">
+              Works at: {user.company.name}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
