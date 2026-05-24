3a8da46c0013398d006c3f471ccbb13b47f3455f
Oleg Onyshchenko <seesmwork@gmail.com>
2026-05-24 17:04:19 +0300

    feat: learning map reduce and filter methods

--- a.js
+++ a.js
@@ -1,4 +1,16 @@
-const user = { name: "Oleh", email: "cumow@wegabba.mz", role: "admin" };
+const products = [
+  { id: 1, title: "Laptop", price: 1000, category: "Electronics" },
+  { id: 2, title: "Shirt", price: 25, category: "Clothing" },
+  { id: 3, title: "Headphones", price: 150, category: "Electronics" },
+];
 
-const { name, email, role } = user;
-console.log(`Name is ${name}.`);
+const productTitles = products.map((product) => product.title);
+console.log(productTitles);
+
+const productsWithoutOne = products.filter((product) => product.id !== 1);
+console.log(productsWithoutOne);
+
+const totalPrice = products.reduce((accumulator, product) => {
+  return accumulator + product.price;
+}, 0);
+console.log(totalPrice);
