f316365345bc31ca6cfd42533c636ff35084e8ab
Oleg Onyshchenko <seesmwork@gmail.com>
2026-05-24 16:32:16 +0300

    feat: fetching users JEsus thank you

--- a.js
+++ a.js
@@ -1,17 +1,18 @@
-const getTodoItem = async () => {
-  try {
-    const response = await fetch(
-      "https://jsonplaceholder.typicode.com/todos/1",
-    );
+const API_URL = "https://jsonplaceholder.typicode.com/users";
 
-    if (!response.ok) throw new Error(`HTTP Error! Status ${response.status}`);
+const fetchUsers = async () => {
+  try {
+    const response = await fetch(API_URL);
+
+    if (!response.ok)
+      throw new Error(`Failed to fetch users. ${response.status}`);
 
     const data = await response.json();
 
-    console.log(`Here is our data: ${data.title}`);
+    console.log(data);
   } catch (error) {
-    console.error(`Something went wrong: ${error.message}`);
+    console.error(`Failed to fetch users: ${error.message}`);
   }
 };
 
-getTodoItem();
+fetchUsers();
