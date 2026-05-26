b966509cd2bcf8d9c279e651e27bf93de303a837
Oleg Onyshchenko <seesmwork@gmail.com>
2026-05-26 09:50:20 +0300

    feat: add menu items

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,6 +4,31 @@ import { Menu } from "@deemlol/next-icons";
 import Link from "next/link";
 import { useState } from "react";
 
+interface MenuItemProps {
+  href: string;
+  content: string;
+  isSelected: boolean;
+}
+
+const MenuItem = ({ href, content, isSelected = false }: MenuItemProps) => {
+  return (
+    <Link
+      className={`${isSelected ? "bg-yellow-50 text-yellow-600" : ""} p-2 hover:bg-yellow-50 hover:text-yellow-600`}
+      href={href}
+    >
+      {content}
+    </Link>
+  );
+};
+
+const links: MenuItemProps[] = [
+  { href: "/", content: "Home", isSelected: true },
+  { href: "/ai", content: "AI & Monorepos", isSelected: false },
+  { href: "/synthetic", content: "Synthetic Monorepos", isSelected: false },
+  { href: "/compare", content: "Compare", isSelected: false },
+  { href: "/ts", content: "TypeScript", isSelected: false },
+];
+
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
 
@@ -13,17 +38,31 @@ export default function Header() {
   };
 
   return (
-    <header className="flex justify-between items-center p-2 px-4 border-b border-stone-100 bg-white/50">
-      <Link href={"/"}>
-        <span className="font-bold">monorepo</span>
-        <span className="text-yellow-500">.tools</span>
-      </Link>
-      <button
-        className="cursor-pointer hover:bg-yellow-50 text-stone-500 hover:text-yellow-500 rounded p-2"
-        onClick={handleMenuOpen}
-      >
-        <Menu />
-      </button>
-    </header>
+    <>
+      <header className="flex justify-between items-center p-2 px-4 border-b border-stone-100 bg-white/50">
+        <Link href={"/"}>
+          <span className="font-bold">monorepo</span>
+          <span className="text-yellow-600">.tools</span>
+        </Link>
+        <button
+          className="cursor-pointer hover:bg-yellow-50 text-stone-600 hover:text-yellow-600 rounded p-2"
+          onClick={handleMenuOpen}
+        >
+          <Menu />
+        </button>
+      </header>
+      {isMenuOpen ? (
+        <div className="bg-white flex flex-col p-3 px-4 gap-1 border-b border-stone-100">
+          {links.map((link, index) => (
+            <MenuItem
+              href={link.href}
+              content={link.content}
+              isSelected={link.isSelected}
+              key={index}
+            />
+          ))}
+        </div>
+      ) : null}
+    </>
   );
 }
