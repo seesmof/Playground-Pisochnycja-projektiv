2c9110e0fd406316f9e3b4d4db3de9575525e37b
Oleg Onyshchenko <seesmwork@gmail.com>
2026-05-26 18:32:36 +0300

    feat: create a server

--- a.js
+++ a.js
@@ -1,16 +1,10 @@
-const products = [
-  { id: 1, title: "Laptop", price: 1000, category: "Electronics" },
-  { id: 2, title: "Shirt", price: 25, category: "Clothing" },
-  { id: 3, title: "Headphones", price: 150, category: "Electronics" },
-];
+import http from "http";
 
-const productTitles = products.map((product) => product.title);
-console.log(productTitles);
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "content-type": "text/plain" });
+  res.end("Here is a server in Node.js.");
+});
 
-const productsWithoutOne = products.filter((product) => product.id !== 1);
-console.log(productsWithoutOne);
-
-const totalPrice = products.reduce((accumulator, product) => {
-  return accumulator + product.price;
-}, 0);
-console.log(totalPrice);
+server.listen(3000, () =>
+  console.log("Server is running on http://localhost:3000"),
+);
