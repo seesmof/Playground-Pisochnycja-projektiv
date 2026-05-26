9a8877ad679eea7bbffdd6b6e6ed0115035123e5
Oleg Onyshchenko <seesmwork@gmail.com>
2026-05-26 13:43:07 +0300

    feat: add the addition form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,32 @@ export default function HomePage() {
     "banana",
     "orange",
     "pear",
-    "strawbery",
+    "strawberry",
+    "raspberry",
   ]);
+  const [input, setInput] = useState<string>("");
+
+  const handleSubmit = (e: React.SubmitEvent) => {
+    e.preventDefault();
+    const newItems = [...items, input];
+    setItems(newItems);
+    console.log(`Successfully added ${input}.`);
+    setInput("");
+  };
 
   return (
     <div className="mx-auto w-full max-w-3xl">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-3 p-3">
+      <form className="flex flex-row gap-3 p-3" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="flex-1 input"
+          placeholder="Item description..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button className="btn">Add</button>
+      </form>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-3 px-3">
         {items.map((item, index) => (
           <div
             className="bg-white p-3 rounded-md outline outline-stone-300 justify-between flex flex-row"
