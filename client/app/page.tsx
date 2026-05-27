45952cad882641edb00d327c6ae6235901e2f313
Oleg Onyshchenko <seesmwork@gmail.com>
2026-05-27 11:42:59 +0300

    feat: experimenting with state

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,9 +1,9 @@
-import AutocompletePage from "@/components/AutocompletePage";
+import Form from "@/components/Form";
 
 export default function Page() {
   return (
     <div className="min-h-screen bg-sky-50 p-3">
-      <AutocompletePage />
+      <Form />
     </div>
   );
 }
