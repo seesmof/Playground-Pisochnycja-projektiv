c47410151d3322b4e67f8851f1692c752739a6e7
Oleg Onyshchenko <seesmwork@gmail.com>
2026-05-30 11:36:55 +0300

    feat: finish the conditional rendering

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,19 +3,31 @@
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
 
+const containerClasses = "flex flex-row justify-between items-center";
+
 const Page = () => {
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
 
+  const handleLogIn = () => {
+    setIsLoggedIn((isLoggedIn) => !isLoggedIn);
+  };
+
   if (!isLoggedIn)
     return (
-      <div className="flex flex-row justify-between">
+      <div className={containerClasses}>
         <p className="text-red-600 font-medium">Please log in first.</p>
-        <Button onClick={() => (isLoggedIn: boolean) => !isLoggedIn}>
-          Log In.
+        <Button onClick={handleLogIn}>Log In.</Button>
+      </div>
+    );
+  else
+    return (
+      <div className={containerClasses}>
+        <p className="font-medium text-lg">Welcome!</p>
+        <Button variant="outline" onClick={handleLogIn}>
+          Log Out.
         </Button>
       </div>
     );
-  else return <p className="font-medium text-lg">Welcome!</p>;
 };
 
 export default Page;
