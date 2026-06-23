c5f69c3cde765e91430e29d15352812cc3deb02c
Oleg Onyshchenko <seesmwork@gmail.com>
2026-06-23 15:02:18 +0300

    feat: make a limited textarea

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,11 +1,27 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Page() {
+  const [len, setLen] = useState<number>(0);
+
   return (
     <div className="bg-sky-50 min-h-screen">
       <div className="flex flex-col gap-3 p-3 mx-auto max-w-3xl">
-        <div className="bg-white rounded-md p-3 text-center text-3xl font-bold">
-          <p className="text-transparent bg-clip-text bg-linear-to-l from-orange-500 via-red-600 to-pink-400">
-            Jesus is KING
-          </p>
+        <div className="bg-white rounded-md p-3 relative">
+          <textarea
+            name="input"
+            id="input"
+            maxLength={250}
+            className={`textarea w-full resize-none ${len === 250 && "text-red-700 outline-red-700"}`}
+            rows={4}
+            onChange={(e) => setLen(e.target.value.length)}
+          ></textarea>
+          <div
+            className={`absolute bottom-5 right-5 ${len === 250 && "text-red-700"}`}
+          >
+            {len}/250
+          </div>
         </div>
       </div>
     </div>
