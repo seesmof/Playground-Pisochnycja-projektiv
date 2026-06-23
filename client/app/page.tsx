a7eddae422823f33efc9d367b03a8bdccf7f8d75
Oleg Onyshchenko <seesmwork@gmail.com>
2026-06-23 12:57:42 +0300

    feat: make a gradient text

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,37 +1,11 @@
-"use client";
-
-import { useState } from "react";
-
-interface Verse {
-  text: string;
-  reference: string;
-}
-
 export default function Page() {
-  const [verses, setVerses] = useState<Verse[]>([
-    {
-      text: "For Whoever shall call upon the name of the LORD shall be saved",
-      reference: "Romans 10:13",
-    },
-    {
-      text: "Jesus wept.",
-      reference: "John 11:35",
-    },
-  ]);
-
   return (
-    <div className="min-h-screen bg-sky-50">
-      <div className="mx-auto flex flex-col gap-3 p-3 max-w-3xl">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3">
-          {verses.map((verse, index) => (
-            <div
-              className="flex flex-col bg-white rounded-md p-3 h-fit"
-              key={index}
-            >
-              <h3 className="font-bold">{verse.reference}</h3>
-              <p className="mt-2">{verse.text}</p>
-            </div>
-          ))}
+    <div className="bg-sky-50 min-h-screen">
+      <div className="flex flex-col gap-3 p-3 mx-auto max-w-3xl">
+        <div className="bg-white rounded-md p-3 text-center text-3xl font-bold">
+          <p className="text-transparent bg-clip-text bg-linear-to-l from-orange-500 via-red-600 to-pink-400">
+            Jesus is KING
+          </p>
         </div>
       </div>
     </div>
