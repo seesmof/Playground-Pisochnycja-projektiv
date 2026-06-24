9d38421ee62e26252256d25d1fdb7f62aa83fc69
Oleg Onyshchenko <seesmwork@gmail.com>
2026-06-24 11:29:25 +0300

    feat: add the cookies notice

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,41 +1,51 @@
-interface ChangelogItemProps {
-  date: string;
-  description: string;
-}
+"use client";
 
-const ChangelogItem = ({ date, description }: ChangelogItemProps) => {
-  return (
-    <div className="flex flex-row relative">
-      <p className="border-r-2 pr-6">{date}</p>
-      <div className="rounded-full h-4 w-4 bg-stone-900 absolute top-1 left-[9.56rem]"></div>
-      <p className="pl-6">{description}</p>
-    </div>
-  );
-};
-
-const items: ChangelogItemProps[] = [
-  {
-    date: "September 3, 2024",
-    description: "Announcing Projects on Frontend Roadmap",
-  },
-  {
-    date: "August 28, 2024",
-    description: "Build your learning habits with learning streaks",
-  },
-];
+import { useState } from "react";
+import { FaCookieBite } from "react-icons/fa";
+import { RxCross1 } from "react-icons/rx";
 
 export default function Page() {
+  const [isHidden, setIsHidden] = useState<boolean>(false);
+
   return (
-    <div className="bg-sky-50 min-h-screen flex items-center justify-center">
-      <div className="bg-white p-3">
-        <h1>Changelog</h1>
-        <p>Here`s everything we have shipped in the past few days.</p>
-        <div className="flex flex-col">
-          {items.map((item, index) => (
-            <ChangelogItem {...item} key={index} />
-          ))}
-        </div>
+    <div className="min-h-screen bg-sky-50">
+      <div className="flex mx-auto max-w-3xl p-3">
+        <Button onClick={() => setIsHidden(!isHidden)}>
+          {isHidden ? "Show" : "Hide"}
+        </Button>
       </div>
+      {!isHidden && (
+        <div className="fixed bottom-3 right-3 bg-stone-900 text-stone-50 p-6 max-w-52 rounded-xl flex flex-col gap-3">
+          <div className="flex justify-between items-center mb-4">
+            <FaCookieBite />
+            <button
+              className="cursor-pointer"
+              onClick={() => setIsHidden((isHidden) => !isHidden)}
+            >
+              <RxCross1 />
+            </button>
+          </div>
+          <p>We use cookies to imporove your user experience.</p>
+          <Button>I like Cookies</Button>
+        </div>
+      )}
     </div>
   );
 }
+
+const Button = ({
+  children,
+  onClick,
+}: {
+  children: React.ReactNode;
+  onClick?: any;
+}) => {
+  return (
+    <button
+      className="bg-stone-50 text-stone-900 w-full rounded-md py-2 cursor-pointer duration-150 hover:bg-stone-100 active:scale-95"
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
