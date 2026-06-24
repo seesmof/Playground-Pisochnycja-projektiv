b0f2f8974de338b3f865ce2f2af0c6bbe8fa7d90
Oleg Onyshchenko <seesmwork@gmail.com>
2026-06-24 10:31:36 +0300

    feat: failed to do a changelog component

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,27 +1,39 @@
-"use client";
+interface ChangelogItemProps {
+  date: string;
+  description: string;
+}
 
-import { useState } from "react";
+const ChangelogItem = ({ date, description }: ChangelogItemProps) => {
+  return (
+    <div className="flex flex-row relative">
+      <p className="border-r-2 pr-6">{date}</p>
+      <div className="rounded-full h-4 w-4 bg-stone-900 absolute top-1 left-[9.56rem]"></div>
+      <p className="pl-6">{description}</p>
+    </div>
+  );
+};
+
+const items: ChangelogItemProps[] = [
+  {
+    date: "September 3, 2024",
+    description: "Announcing Projects on Frontend Roadmap",
+  },
+  {
+    date: "August 28, 2024",
+    description: "Build your learning habits with learning streaks",
+  },
+];
 
 export default function Page() {
-  const [len, setLen] = useState<number>(0);
-
   return (
-    <div className="bg-sky-50 min-h-screen">
-      <div className="flex flex-col gap-3 p-3 mx-auto max-w-3xl">
-        <div className="bg-white rounded-md p-3 relative">
-          <textarea
-            name="input"
-            id="input"
-            maxLength={250}
-            className={`textarea w-full resize-none ${len === 250 && "text-red-700 outline-red-700"}`}
-            rows={4}
-            onChange={(e) => setLen(e.target.value.length)}
-          ></textarea>
-          <div
-            className={`absolute bottom-5 right-5 ${len === 250 && "text-red-700"}`}
-          >
-            {len}/250
-          </div>
+    <div className="bg-sky-50 min-h-screen flex items-center justify-center">
+      <div className="bg-white p-3">
+        <h1>Changelog</h1>
+        <p>Here`s everything we have shipped in the past few days.</p>
+        <div className="flex flex-col">
+          {items.map((item, index) => (
+            <ChangelogItem {...item} key={index} />
+          ))}
         </div>
       </div>
     </div>
