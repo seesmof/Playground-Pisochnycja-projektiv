710912ee70ce397a6adb1936117e067a3114a3d6
Oleg Onyshchenko <seesmwork@gmail.com>
2026-06-25 21:40:03 +0300

    feat: add the placeholder root route

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
-
 const Bible = require("../bible.json")
 
 const app = express();
 const port = 3000;
+
+app.get("/", (req, res) => {
+    return res.status(200).send("Please enter a Bible place in a format /Book/Chater/Verse or just /Book/Chapter/")
+})
 
 app.get("/:book/:chapter", (req, res) => {
     const book = req.params.book;
@@ -15,8 +18,25 @@ app.get("/:book/:chapter", (req, res) => {
     if (!BibleBooks.includes(book)) return res.status(404).send("The provided book name was not found. Make sure the format is 3-letters capitalized: GEN, ROM, 1CO.")
 
     const result = Bible[book][chapter];
+    if (!result || result === null) return res.status(404).send("Couldn't find the chapter specified. Please check the URL to make sure it's in the correct format.")
 
-    return res.status(200).send(result);
+    return res.status(200).json(result);
+})
+
+app.get("/:book/:chapter/:verse", (req, res) => {
+    const book = req.params.book;
+    let chapter = req.params.chapter;
+    const verse = req.params.verse;
+
+    if (!chapter || chapter < 1 || chapter > 150) chapter = 1;
+
+    const BibleBooks = Object.keys(Bible);
+    if (!BibleBooks.includes(book)) return res.status(404).send("The provided book name was not found. Make sure the format is 3-letters capitalized: GEN, ROM, 1CO.")
+
+    const result = Bible[book][chapter][verse];
+    if (!result || result === null) return res.status(404).send("Couldn't find the verse provided. Please check the URL to make sure it is in the correct format.")
+
+    return res.status(200).json(result);
 })
 
 app.listen(port)
