07ae860c3f2ff0a84123f08d705f7f36f87a1ada
Oleg Onyshchenko <seesmwork@gmail.com>
2026-06-25 21:28:03 +0300

    feat: make the Bible chapter fetching

--- server/server.js
+++ server/server.js
@@ -7,10 +7,16 @@ const port = 3000;
 
 app.get("/:book/:chapter", (req, res) => {
     const book = req.params.book;
-    const chapter = req.params.chapter;
+    let chapter = req.params.chapter;
+
+    if (!chapter || chapter < 1 || chapter > 150) chapter = 1;
 
     const BibleBooks = Object.keys(Bible);
     if (!BibleBooks.includes(book)) return res.status(404).send("The provided book name was not found. Make sure the format is 3-letters capitalized: GEN, ROM, 1CO.")
+
+    const result = Bible[book][chapter];
+
+    return res.status(200).send(result);
 })
 
 app.listen(port)
