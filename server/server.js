ee4dbf3b8fbfdd994a6b8d95717a8d3f9f699ca8
Oleg Onyshchenko <seesmwork@gmail.com>
2026-06-25 21:15:02 +0300

    feat: start working on an express API

--- server/server.js
+++ server/server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 
+const Bible = require("../bible.json")
+
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-    console.log("Jesus is LORD")
-    res.status(500).json({ name: "hey", "message": "error", })
+app.get("/:book/:chapter", (req, res) => {
+    const book = req.params.book;
+    const chapter = req.params.chapter;
+
+    const BibleBooks = Object.keys(Bible);
+    if (!BibleBooks.includes(book)) return res.status(404).send("The provided book name was not found. Make sure the format is 3-letters capitalized: GEN, ROM, 1CO.")
 })
 
 app.listen(port)
