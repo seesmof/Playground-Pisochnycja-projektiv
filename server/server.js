3779fb0ea1eb14758a03fcfa643a32bf367d6ff1
Oleg Onyshchenko <seesmwork@gmail.com>
2026-06-26 13:41:20 +0300

    feat: doing nothing

--- server/server.js
+++ server/server.js
@@ -1,42 +1,18 @@
 const express = require("express");
-const Bible = require("../bible.json")
+const cors = require("cors");
 
 const app = express();
-const port = 3000;
+app.use(cors());
+const port = 8080;
+
+let todos = [];
 
 app.get("/", (req, res) => {
-    return res.status(200).send("Please enter a Bible place in a format /Book/Chater/Verse or just /Book/Chapter/")
+    return res.status(200).json()
 })
 
-app.get("/:book/:chapter", (req, res) => {
-    const book = req.params.book;
-    let chapter = req.params.chapter;
-
-    if (!chapter || chapter < 1 || chapter > 150) chapter = 1;
-
-    const BibleBooks = Object.keys(Bible);
-    if (!BibleBooks.includes(book)) return res.status(404).send("The provided book name was not found. Make sure the format is 3-letters capitalized: GEN, ROM, 1CO.")
-
-    const result = Bible[book][chapter];
-    if (!result || result === null) return res.status(404).send("Couldn't find the chapter specified. Please check the URL to make sure it's in the correct format.")
-
-    return res.status(200).json(result);
-})
-
-app.get("/:book/:chapter/:verse", (req, res) => {
-    const book = req.params.book;
-    let chapter = req.params.chapter;
-    const verse = req.params.verse;
-
-    if (!chapter || chapter < 1 || chapter > 150) chapter = 1;
-
-    const BibleBooks = Object.keys(Bible);
-    if (!BibleBooks.includes(book)) return res.status(404).send("The provided book name was not found. Make sure the format is 3-letters capitalized: GEN, ROM, 1CO.")
-
-    const result = Bible[book][chapter][verse];
-    if (!result || result === null) return res.status(404).send("Couldn't find the verse provided. Please check the URL to make sure it is in the correct format.")
-
-    return res.status(200).json(result);
+app.get("/", (req, res) => {
+    return res.status(200).send("Jesus is LORD")
 })
 
 app.listen(port)
