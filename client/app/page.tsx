bac57b37755d432bf672e6f1e44b5e4fca0f06c6
Oleg Onyshchenko <seesmwork@gmail.com>
2026-07-03 10:27:42 +0300

    feat: trying and failing

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,40 +3,34 @@
 import { useState } from "react";
 
 export default function Page() {
+  const [nextStep, setNextStep] = useState<"X" | "Y">("X");
+  const [grid, setGrid] = useState([
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ]);
+
   return (
     <div className="p-3 gap-3">
       <h1 className="text-3xl font-bold">Tic Tac Toe</h1>
       <p className="my-4">This is a simple tic tac toe game app.</p>
       <div className="grid grid-cols-3 grid-rows-3 gap-3">
-        <button className="outline active:scale-95 aspect-square cursor-pointer rounded-md">
-          X
-        </button>
-        <button className="outline active:scale-95 aspect-square cursor-pointer rounded-md">
-          X
-        </button>
-        <button className="outline active:scale-95 aspect-square cursor-pointer rounded-md">
-          X
-        </button>
+        <button
+          className="outline-2 active:scale-95 text-3xl aspect-square cursor-pointer rounded-md"
+          onClick={() => {
+            setGrid([[nextStep, ...grid], [...grid], [...grid]]);
+          }}
+        ></button>
+        <button className="outline-2 active:scale-95 text-3xl aspect-square cursor-pointer rounded-md"></button>
+        <button className="outline-2 active:scale-95 text-3xl aspect-square cursor-pointer rounded-md"></button>
 
-        <button className="outline active:scale-95 aspect-square cursor-pointer rounded-md">
-          X
-        </button>
-        <button className="outline active:scale-95 aspect-square cursor-pointer rounded-md">
-          X
-        </button>
-        <button className="outline active:scale-95 aspect-square cursor-pointer rounded-md">
-          X
-        </button>
+        <button className="outline-2 active:scale-95 text-3xl aspect-square cursor-pointer rounded-md"></button>
+        <button className="outline-2 active:scale-95 text-3xl aspect-square cursor-pointer rounded-md"></button>
+        <button className="outline-2 active:scale-95 text-3xl aspect-square cursor-pointer rounded-md"></button>
 
-        <button className="outline active:scale-95 aspect-square cursor-pointer rounded-md">
-          X
-        </button>
-        <button className="outline active:scale-95 aspect-square cursor-pointer rounded-md">
-          X
-        </button>
-        <button className="outline active:scale-95 aspect-square cursor-pointer rounded-md">
-          X
-        </button>
+        <button className="outline-2 active:scale-95 text-3xl aspect-square cursor-pointer rounded-md"></button>
+        <button className="outline-2 active:scale-95 text-3xl aspect-square cursor-pointer rounded-md"></button>
+        <button className="outline-2 active:scale-95 text-3xl aspect-square cursor-pointer rounded-md"></button>
       </div>
     </div>
   );
