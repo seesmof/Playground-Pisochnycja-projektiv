59d113e49ca52dd46c42c5784b416aa672ed9dd8
Oleg Onyshchenko <seesmwork@gmail.com>
2026-07-03 10:33:00 +0300

    feat: make the grid items clickable

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,8 +2,11 @@
 
 import { useState } from "react";
 
+const buttonClasses =
+  "outline-2 active:scale-95 text-3xl aspect-square cursor-pointer rounded-md";
+
 export default function Page() {
-  const [nextStep, setNextStep] = useState<"X" | "Y">("X");
+  const [nextStep, setNextStep] = useState<"X" | "O">("X");
   const [grid, setGrid] = useState([
     ["", "", ""],
     ["", "", ""],
@@ -16,21 +19,106 @@ export default function Page() {
       <p className="my-4">This is a simple tic tac toe game app.</p>
       <div className="grid grid-cols-3 grid-rows-3 gap-3">
         <button
-          className="outline-2 active:scale-95 text-3xl aspect-square cursor-pointer rounded-md"
+          className={buttonClasses}
           onClick={() => {
-            setGrid([[nextStep, ...grid], [...grid], [...grid]]);
+            const newGrid = grid;
+            newGrid[0][0] = nextStep;
+            setGrid(newGrid);
+            setNextStep(nextStep === "X" ? "O" : "X");
           }}
-        ></button>
-        <button className="outline-2 active:scale-95 text-3xl aspect-square cursor-pointer rounded-md"></button>
-        <button className="outline-2 active:scale-95 text-3xl aspect-square cursor-pointer rounded-md"></button>
+        >
+          {grid[0][0]}
+        </button>
+        <button
+          className={buttonClasses}
+          onClick={() => {
+            const newGrid = grid;
+            newGrid[0][1] = nextStep;
+            setGrid(newGrid);
+            setNextStep(nextStep === "X" ? "O" : "X");
+          }}
+        >
+          {grid[0][1]}
+        </button>
+        <button
+          className={buttonClasses}
+          onClick={() => {
+            const newGrid = grid;
+            newGrid[0][2] = nextStep;
+            setGrid(newGrid);
+            setNextStep(nextStep === "X" ? "O" : "X");
+          }}
+        >
+          {grid[0][2]}
+        </button>
 
-        <button className="outline-2 active:scale-95 text-3xl aspect-square cursor-pointer rounded-md"></button>
-        <button className="outline-2 active:scale-95 text-3xl aspect-square cursor-pointer rounded-md"></button>
-        <button className="outline-2 active:scale-95 text-3xl aspect-square cursor-pointer rounded-md"></button>
+        <button
+          className={buttonClasses}
+          onClick={() => {
+            const newGrid = grid;
+            newGrid[1][0] = nextStep;
+            setGrid(newGrid);
+            setNextStep(nextStep === "X" ? "O" : "X");
+          }}
+        >
+          {grid[1][0]}
+        </button>
+        <button
+          className={buttonClasses}
+          onClick={() => {
+            const newGrid = grid;
+            newGrid[1][1] = nextStep;
+            setGrid(newGrid);
+            setNextStep(nextStep === "X" ? "O" : "X");
+          }}
+        >
+          {grid[1][1]}
+        </button>
+        <button
+          className={buttonClasses}
+          onClick={() => {
+            const newGrid = grid;
+            newGrid[1][2] = nextStep;
+            setGrid(newGrid);
+            setNextStep(nextStep === "X" ? "O" : "X");
+          }}
+        >
+          {grid[1][2]}
+        </button>
 
-        <button className="outline-2 active:scale-95 text-3xl aspect-square cursor-pointer rounded-md"></button>
-        <button className="outline-2 active:scale-95 text-3xl aspect-square cursor-pointer rounded-md"></button>
-        <button className="outline-2 active:scale-95 text-3xl aspect-square cursor-pointer rounded-md"></button>
+        <button
+          className={buttonClasses}
+          onClick={() => {
+            const newGrid = grid;
+            newGrid[2][0] = nextStep;
+            setGrid(newGrid);
+            setNextStep(nextStep === "X" ? "O" : "X");
+          }}
+        >
+          {grid[2][0]}
+        </button>
+        <button
+          className={buttonClasses}
+          onClick={() => {
+            const newGrid = grid;
+            newGrid[2][1] = nextStep;
+            setGrid(newGrid);
+            setNextStep(nextStep === "X" ? "O" : "X");
+          }}
+        >
+          {grid[2][1]}
+        </button>
+        <button
+          className={buttonClasses}
+          onClick={() => {
+            const newGrid = grid;
+            newGrid[2][2] = nextStep;
+            setGrid(newGrid);
+            setNextStep(nextStep === "X" ? "O" : "X");
+          }}
+        >
+          {grid[2][2]}
+        </button>
       </div>
     </div>
   );
