3c802d48e10334022c7f047078699ab17e24d109
Oleg Onyshchenko <seesmwork@gmail.com>
2026-07-03 10:18:46 +0300

    feat: add a button

--- a.js
+++
@@ -1,6 +0,0 @@
-const data = {
-  "1": "In the beginning God created the heavens and the earth.",
-  "2": "And the earth was formless and void, and the Spirit of God was brooding over the waters."
-}
-
-console.log(data["1"])
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,26 +3,9 @@
 import { useState } from "react";
 
 export default function Page() {
-  const [input, setInput] = useState<string>("");
-
   return (
-    <div className="p-3 flex flex-col gap-3">
-      <input
-        type="text"
-        className="input w-full"
-        placeholder="Enter your request..."
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      />
-      <button className="btn">Parse</button>
-      <textarea
-        disabled
-        className="textarea w-full resize-none"
-        rows={3}
-        name="output"
-        id="outputTextarea"
-        value={input}
-      />
+    <div className="p-3 gap-3">
+      <button className="outline py-3 px-4 cursor-pointer rounded-md">X</button>
     </div>
   );
 }
