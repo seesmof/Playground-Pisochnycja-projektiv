030bf9221b1e98d01e1b6b68d9e9f897b1a08535
Oleg Onyshchenko <seesmwork@gmail.com>
2026-07-16 17:26:25 +0300

    feat: add the Bible fetching in nextjs backend

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,8 +1,3 @@
-const baseUrl = "https://open-bible-api.vercel.app/PSA/";
-
-export default async function IndexPage() {
-  const chapter = 
-  const data = await fetch();
-
-  return <div className="p-3 mx-auto max-w-3xl"></div>;
+export default function Page() {
+  return "Jesus is LORD";
 }
