02be63650bbd1270c70589fc813c0becd29a5c1c
Oleg Onyshchenko <seesmwork@gmail.com>
2026-07-17 18:20:52 +0300

    feat: add directions

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,3 +1,25 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Page() {
-  return "Jesus is LORD";
+  const [direction, setDirection] = useState<"Up" | "Right" | "Down" | "Left">(
+    "Up",
+  );
+
+  return (
+    <div className="p-3">
+      {direction === "Up" ? (
+        <p>/\</p>
+      ) : direction === "Right" ? (
+        <p>&lt;</p>
+      ) : direction === "Down" ? (
+        <p>\/</p>
+      ) : direction === "Left" ? (
+        <p>&gt;</p>
+      ) : (
+        "Invalid direction."
+      )}
+    </div>
+  );
 }
