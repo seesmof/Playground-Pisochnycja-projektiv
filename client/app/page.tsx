8c1cb27c007e955814f68fa47d11c98a29201cae
Oleg Onyshchenko <seesmwork@gmail.com>
2026-07-17 18:25:06 +0300

    chore: direction change on key press doesnt work

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,11 +1,29 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Page() {
   const [direction, setDirection] = useState<"Up" | "Right" | "Down" | "Left">(
     "Up",
   );
+
+  const handleKeyDown = (event: KeyboardEvent) => {
+    if (event.key === "ArrowUp") {
+      setDirection("Up");
+    } else if (event.key === "ArrowRight") {
+      setDirection("Right");
+    } else if (event.key === "ArrowDown") {
+      setDirection("Down");
+    } else if (event.key === "ArrowLeft") {
+      setDirection("Left");
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, []);
 
   return (
     <div className="p-3">
