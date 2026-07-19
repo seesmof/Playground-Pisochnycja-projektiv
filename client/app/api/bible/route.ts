07084a2e2b009b8483efd3f5c99a5281a912998d
Oleg Onyshchenko <seesmwork@gmail.com>
2026-07-19 17:00:13 +0300

    chore: experimenting with prisma

--- client/app/api/bible/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-import { promises as fs } from "fs";
-
-export async function GET() {
-  const file = await fs.readFile(process.cwd() + "../../Bible.json", "utf8");
-  const data = JSON.parse(file);
-  return NextResponse.json({ data: data });
-}
