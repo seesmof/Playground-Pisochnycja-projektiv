b37ded9b22e21855d35eb8e9530c4384e738b87a
Oleg Onyshchenko <seesmwork@gmail.com>
2026-07-31 11:10:08 +0300

    refactor: remove asp.net and django

--- django/client/frontend/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
-
-
-# Create your views here.
-def index(req):
-    data = {"message": "Jesus is LORD", "status": 200}
-    return JsonResponse(data)
--- server/app.js
+++
@@ -1,9 +0,0 @@
-let http = require("http");
-let os = require("os");
-
-http
-  .createServer((req, res) => {
-    res.writeHead(200, { "content-type": "text/plain" });
-    res.end(os.platform());
-  })
-  .listen(8080);
