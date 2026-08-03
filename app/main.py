8dde931663a79f2d2ce3025a767a7d81f1fa9098
Oleg Onyshchenko <seesmwork@gmail.com>
2026-08-03 18:04:02 +0300

    feat: make a good onclick, praise JESUS!

--- app/main.py
+++ app/main.py
@@ -12,9 +12,15 @@ class State:
     cart_items: list[CartItme] = []
 
 
+def add_new_item():
+    new_cart: CartItme = CartItme(name="iPhone 15 Max", price=1000)
+    State.cart_items.append(new_cart)
+    main_page.refresh()
+
+
 @ui.refreshable
 def main_page():
-    ui.button(on_click=...)
+    ui.button("Add", on_click=add_new_item)
 
     for item in State.cart_items:
         ui.label(f"{item.name} costs ${item.price}")
