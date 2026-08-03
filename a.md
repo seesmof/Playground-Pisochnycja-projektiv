44873fc74f3ca1c10623c2e5b5cc2db9776a17d3
Oleg Onyshchenko <seesmwork@gmail.com>
2026-08-03 18:30:52 +0300

    fail to have a global state, Jesus please help

---
+++ a.md
@@ -0,0 +1,1 @@
+There should be a main page with a grid of items and a button to add each to cart.
---
+++ client/data/cart.ts
@@ -0,0 +1,6 @@
+export interface CartItem {
+  name: string;
+  price: number;
+}
+
+export let cart: CartItem[] = [];
