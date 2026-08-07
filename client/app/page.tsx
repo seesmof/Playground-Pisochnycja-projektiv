9477ad2da571bbe0fdc3e5a5b0d61a5bfa265797
Oleg Onyshchenko <seesmwork@gmail.com>
2026-08-07 14:52:16 +0300

    chore: failed with SQLite

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,47 +1,31 @@
 "use client";
 
-import Navbar from "@/components/Navbar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-export interface CartItem {
-  name: string;
-  price: number;
-}
+export default function Page() {
+  const [users, setUsers] = useState([]);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
 
-export default function IndexPage() {
-  const [cartItems, setCartItems] = useState<CartItem[]>([]);
-  const [items, setItems] = useState<CartItem[]>([
-    { name: "iPhone 15 Max", price: 1000 },
-    { name: "MacBook Air", price: 1500 },
-    { name: "Air Pods Pro", price: 500 },
-  ]);
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch("http://localhost:5000/api/users");
+        const data = await response.json();
+        setUsers(data);
+      } catch (error) {
+        console.error(`Failed to fetch data: ${error}`);
+      }
+    };
+
+    fetchUsers();
+  }, []);
 
   return (
-    <div className="min-h-screen bg-sky-50">
-      <Navbar cartItems={cartItems} />
-      <div className="mx-auto max-w-md w-full gap-3 p-3 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-        {items.map((item, index) => (
-          <div
-            key={index}
-            className="rounded-md bg-white p-3 outline outline-sky-600 flex flex-col gap-1"
-          >
-            <h2 className="text-lg">{item.name}</h2>
-            <p>$ {item.price}</p>
-            <button
-              className="bg-sky-600 text-white rounded-md mt-3 active:scale-95 cursor-pointer hover:bg-sky-700"
-              onClick={() => {
-                const newItem: CartItem = {
-                  name: item.name,
-                  price: item.price,
-                };
-                setCartItems([...cartItems, newItem]);
-              }}
-            >
-              Add to cart
-            </button>
-          </div>
-        ))}
-      </div>
+    <div className="p-5 font-sans">
+      <h1>Users Management (React & SQLite)</h1>
+
+      <form></form>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,11 +1,35 @@
-const http = require("http");
-const PORT = 3000;
+const express = require("express");
+const cors = require("cors");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const path = require("path");
 
-const server = http.createServer((req, res) => {
-  res.write("Jesus is LORD");
-  res.end();
-});
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-server.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`);
-});
+let db;
+
+async function initializeDbAndServer() {
+  try {
+    db = await open({
+      filename: path.join(__dirname, "database.db"),
+      driver: sqlite3.Database,
+    });
+
+    await db.exec(`
+      CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        email TEXT UNIQUE NOT NULL
+      );
+    `);
+
+    app.listen(5000, () => {
+      console.log("Server started on http://localhost:5000");
+    });
+  } catch (error) {
+    console.error(`A database failed: ${error.message}`);
+    process.exit(1);
+  }
+}
