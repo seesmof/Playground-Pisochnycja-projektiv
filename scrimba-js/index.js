7bec92ad28c0a5218c294a64463d74451a476e61
Oleg Onyshchenko <seesmwork@gmail.com>
2026-08-07 11:28:20 +0300

    feat: add a counter app in js

--- scrimba-js/index.js
+++ scrimba-js/index.js
@@ -1,1 +1,20 @@
-const counter = (document.getElementById("count-el").innerText = 5);
+let count = 0;
+
+const countElement = document.querySelector("#count");
+const minusButton = document.querySelector("#minus");
+const plusButton = document.querySelector("#plug");
+
+const updateCount = () => {
+  countElement.innerHTML = count;
+};
+
+minusButton.addEventListener("click", () => {
+  console.log("Minus");
+  count--;
+  updateCount();
+});
+
+plusButton.addEventListener("click", () => {
+  count++;
+  updateCount();
+});
