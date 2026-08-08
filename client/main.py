165258c31455c7eb52557ba49da024a5aee3ab93
Oleg Onyshchenko <seesmwork@gmail.com>
2026-08-08 10:29:13 +0300

    chore: unclear django

--- client/main.py
+++
@@ -1,5 +0,0 @@
-from nicegui import ui
-
-ui.label("Hi")
-
-ui.run(favicon="🏬", title="Shopper")
---
+++ server/myapp/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from myapp.models import Task
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ["id", "title", "description", "is_completed"]
