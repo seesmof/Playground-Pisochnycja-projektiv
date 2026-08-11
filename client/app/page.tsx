f4a6d9cafe1dd6e060faf3a8eaa5dfd2f7a49f98
Oleg Onyshchenko <seesmwork@gmail.com>
2026-08-11 19:55:53 +0300

    chore: add a resizable panel but with no min width, praise Jesus

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,32 +1,20 @@
-import { Button } from "@/components/ui/button";
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import Link from "next/link";
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
 
 export default function IndexPage() {
   return (
     <div className="flex bg-sky-50 min-h-screen items-center justify-center px-4">
-      <Card className="w-full max-w-md">
-        <CardHeader>
-          <CardTitle className="text-3xl font-bold">An auth test</CardTitle>
-          <CardDescription>
-            This is a simple app to test authentication in Next.js.
-          </CardDescription>
-        </CardHeader>
-        <CardContent className="flex gap-3">
-          <Link href={"/login"}>
-            <Button>Log In</Button>
-          </Link>
-          <Link href={"/signup"}>
-            <Button>Sign Up</Button>
-          </Link>
-        </CardContent>
-      </Card>
+      <ResizablePanelGroup
+        className="bg-white rounded-md max-w-md"
+        orientation="horizontal"
+      >
+        <ResizablePanel className="p-3">Sidebar</ResizablePanel>
+        <ResizableHandle />
+        <ResizablePanel className="p-3">Main Panel</ResizablePanel>
+      </ResizablePanelGroup>
     </div>
   );
 }
