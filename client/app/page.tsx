901e02dbe39d41ff93dba7622fac253f7850388b
Oleg Onyshchenko <seesmwork@gmail.com>
2026-08-11 20:13:43 +0300

    chore: failing to make a sidebar

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,20 +1,8 @@
-import {
-  ResizableHandle,
-  ResizablePanel,
-  ResizablePanelGroup,
-} from "@/components/ui/resizable";
-
 export default function IndexPage() {
   return (
-    <div className="flex bg-sky-50 min-h-screen items-center justify-center px-4">
-      <ResizablePanelGroup
-        className="bg-white rounded-md max-w-md"
-        orientation="horizontal"
-      >
-        <ResizablePanel className="p-3">Sidebar</ResizablePanel>
-        <ResizableHandle />
-        <ResizablePanel className="p-3">Main Panel</ResizablePanel>
-      </ResizablePanelGroup>
+    <div className="min-h-screen flex overflow-hidden">
+      <aside className="w-64 shrink-0 bg-gray-800 text-white">Amen</aside>
+      <main className="flex-1 overflow-y-auto bg-gray-100"></main>
     </div>
   );
 }
