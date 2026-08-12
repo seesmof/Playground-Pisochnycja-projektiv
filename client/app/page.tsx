6101690bf74d185fdf2d04ef6de65308ed5cddc2
Oleg Onyshchenko <seesmwork@gmail.com>
2026-08-12 08:42:17 +0300

    feat: make a fixed sidebar

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,8 +1,10 @@
 export default function IndexPage() {
   return (
     <div className="min-h-screen flex overflow-hidden">
-      <aside className="w-64 shrink-0 bg-gray-800 text-white">Amen</aside>
-      <main className="flex-1 overflow-y-auto bg-gray-100"></main>
+      <aside className="w-60 bg-gray-800 text-white p-3 h-screen fixed">
+        Amen
+      </aside>
+      <main className="flex-1 overflow-y-auto bg-gray-100 ml-60 p-3"></main>
     </div>
   );
 }
