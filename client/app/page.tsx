45118014793b0e5019e53c81bab1d53ce1789c9e
Oleg Onyshchenko <seesmwork@gmail.com>
2026-08-18 16:30:06 +0300

    feat: add the item setter

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,33 +1,33 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
 import { Item, useCart } from "@/store/cart";
 
 export default function IndexPage() {
-  const { items, add, remove, update } = useCart();
+  const { items, add } = useCart();
 
   return (
     <div className="min-h-screen flex overflow-hidden">
-      <aside className="w-60 bg-gray-800 text-white p-3 h-screen fixed">
-        Amen
-      </aside>
-      <main className="flex-1 overflow-y-auto bg-gray-100 ml-60 p-3"></main>
+      <aside className="w-60 p-3 h-screen fixed border-r-2">Amen</aside>
+      <main className="flex-1 overflow-y-auto ml-60 p-3">
+        <Button
+          onClick={() => {
+            const newItem: Item = {
+              id: crypto.randomUUID(),
+              name: "iPhone 15 Pro",
+              price: 1000,
+            };
+            add(newItem);
+          }}
+        >
+          Add
+        </Button>
+        {items.map((item) => (
+          <div key={item.id}>
+            {item.id}. {item.name} costs ${item.price}.
+          </div>
+        ))}
+      </main>
     </div>
   );
 }
--- client/store/cart.ts
+++ client/store/cart.ts
@@ -10,17 +10,10 @@ type CartState = {
   items: Item[];
   add: (item: Item) => void;
   remove: (id: string) => void;
-  update: (id: string, updates: Partial<Item>) => void;
 };
 
 export const useCart = create<CartState>((set) => ({
-  items: [],
+  items: [{ id: "1", name: "iPhone 15", price: 1000 }],
   add: (item) => set((s) => ({ items: [...s.items, item] })),
   remove: (id) => set((s) => ({ items: s.items.filter((i) => i.id !== id) })),
-  update: (id, updates) =>
-    set((s) => ({
-      items: s.items.map((item) =>
-        item.id === id ? { ...item, ...updates } : item,
-      ),
-    })),
 }));
