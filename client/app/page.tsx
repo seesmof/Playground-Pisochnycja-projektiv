a08a246a168a1f21bc5dbefa35bd211422544a89
Oleg Onyshchenko <seesmwork@gmail.com>
2026-08-20 18:31:38 +0300

    feat: add a navbar with theme toggle

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,27 @@
+"use client";
+
+import { Moon, Sun } from "lucide-react";
+import { useState } from "react";
+
 export default function IndexPage() {
+  const [isDark, setIsDark] = useState<boolean>(false);
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <p>00:00</p>
+    <div
+      className={`min-h-screen ${isDark ? "bg-slate-700 text-white" : "bg-stone-50"}`}
+    >
+      <nav
+        className={`sticky top-0 border-b-2 ${isDark ? "bg-slate-800 border-slate-900" : "bg-white"}`}
+      >
+        <div className="container mx-auto flex items-center justify-between px-4 py-3">
+          <h1 className="font-bold">Where in the world?</h1>
+          <button
+            className="cursor-pointer p-3"
+            onClick={() => setIsDark((isDark) => !isDark)}
+          >
+            {isDark ? <Sun /> : <Moon />}
+          </button>
+        </div>
+      </nav>
     </div>
   );
 }
---
+++ page.tsx
@@ -0,0 +1,7 @@
+export default function IndexPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <p>00:00</p>
+    </div>
+  );
+}
