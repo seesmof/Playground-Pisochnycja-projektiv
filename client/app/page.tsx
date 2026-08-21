85b2f7f8228f8c644d76070236adb8153029be91
Oleg Onyshchenko <seesmwork@gmail.com>
2026-08-21 18:20:53 +0300

    chore: add some form

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,89 +1,28 @@
-"use client";
-
-import { SubmitHandler, useForm } from "react-hook-form";
-import * as z from "zod";
-
-const FormSchema = z.object({
-  username: z.string(),
-  email: z.string().email(),
-  password: z.string().min(5, "The password cannot be less than 5 symbols"),
-  passwordConfirm: z.string(),
-});
-
-type FormInput = z.infer<typeof FormSchema>;
-
 export default function IndexPage() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<FormInput>({});
-
-  const onSubmit: SubmitHandler<FormInput> = (data) => {
-    console.log("The form was submitted.");
-  };
-
   return (
-    <div className="min-h-screen bg-stone-50">
+    <div className="min-h-screen bg-sky-50">
       <div className="container mx-auto px-4 py-2">
-        <div className="bg-white rounded-md outline outline-stone-100 shadow p-5">
-          {/* Form Itself */}
-          <form onSubmit={handleSubmit(onSubmit)}>
-            {/* Username */}
+        <div className="bg-white rounded-md shadow p-5">
+          <h1 className="font-bold text-2xl">A Simple Login Form</h1>
+          <p className="py-4 mb-4">This is a simple login form.</p>
+          <form>
             <fieldset className="fieldset">
-              <legend className="fieldset-legend">Username</legend>
+              <label htmlFor="usernameInput" className="label">
+                Username
+              </label>
               <input
-                className="input w-full"
                 type="text"
+                className="input w-full"
                 placeholder="Your username here..."
-                {...(register("username"),
-                {
-                  required: true,
-                })}
               />
-              <p className="fieldset-label">{errors.username?.message}</p>
             </fieldset>
-
-            {/* Email */}
-            <fieldset className="fieldset">
-              <legend className="fieldset-legend">Email</legend>
-              <input
-                className="input w-full"
-                type="email"
-                placeholder="somemail@gmail.com"
-                {...(register("email"),
-                {
-                  required: true,
-                })}
-              />
-              <p className="fieldset-label">{errors.email?.message}</p>
+            <fieldset className="fieldset mt-2">
+              <label htmlFor="passwordInput" className="label">
+                Password
+              </label>
+              <input type="password" className="input w-full" />
             </fieldset>
-
-            {/* Password */}
-            <fieldset className="fieldset">
-              <legend className="fieldset-legend">Password</legend>
-              <input
-                className="input w-full"
-                type="password"
-                {...(register("password"), { required: true })}
-              />
-              <p className="fieldset-label">{errors.password?.message}</p>
-            </fieldset>
-
-            {/* Password Cofirm */}
-            <fieldset className="fieldset">
-              <legend className="fieldset-legend">Username</legend>
-              <input
-                className="input w-full"
-                type="password"
-                {...(register("passwordConfirm"), { required: true })}
-              />
-              <p className="fieldset-label">
-                {errors.passwordConfirm?.message}
-              </p>
-            </fieldset>
-
-            <button type="submit" className="btn w-full">
+            <button type="submit" className="w-full btn mt-5">
               Submit
             </button>
           </form>
--- page.tsx
+++
@@ -1,7 +0,0 @@
-export default function IndexPage() {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <p>00:00</p>
-    </div>
-  );
-}
