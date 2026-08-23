63dacfd5c6a4559d223716e0ae858a466389518f
Oleg Onyshchenko <seesmwork@gmail.com>
2026-08-23 16:13:49 +0300

    feat: add initial table data

--- a.md
+++ a.md
@@ -2,4 +2,6 @@ What needs there are for the churh website? A link to the youtube live recording
 
 Optional: we can add a statement of faith, a list of our elders, a news (events) blog, молитовні потреби, giving.
 
-How to make a first transparent but with scroll a white-filled navbar?
+---
+
+How to make a sortable table?
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,12 +1,36 @@
+export type Data = {
+  name: string;
+  email: string;
+  age: number;
+  salary: number;
+};
+
+const initialData: Data[] = [
+  { name: "Aaron Townsend", email: "zufe@kohbe.tn", age: 36, salary: 1808 },
+  { name: "Cecilia Fields", email: "za@ite.gq", age: 31, salary: 2894 },
+  { name: "Nancy Bishop", email: "ohefo@biwniecu.tc", age: 54, salary: 2963 },
+  { name: "Lucille Lawrence", email: "zi@hi.wf", age: 55, salary: 1649 },
+  { name: "Maud Sutton", email: "wa@zuf.pe", age: 60, salary: 1629 },
+  { name: "Clayton Moore", email: "udzo@ven.bw", age: 47, salary: 2858 },
+  { name: "Allie Norton", email: "ew@oto.ht", age: 55, salary: 2013 },
+];
+
 export default function Page() {
   return (
-    <div className="grid gap-1 grid-cols-[repeat(auto-fill, 16px)]">
-      {Array.from({ length: 31 }, (_, i) => i + 1).map((day, index) => (
-        <div
-          className="aspect-square bg-stone-300 h-5 w-full col-span-1"
-          key={index}
-        ></div>
-      ))}
+    <div className="bg-sky-50 min-h-screen">
+      <div className="container mx-auto px-4 py-2">
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Age</th>
+              <th>Salary</th>
+            </tr>
+          </thead>
+          <tbody></tbody>
+        </table>
+      </div>
     </div>
   );
 }
