b81c99e3ec168dc0a0af05cdc3101d4372f5eb31
Oleg Onyshchenko <seesmwork@gmail.com>
2026-08-24 21:22:38 +0300

    feat: make the tasks displayed properly

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -6,14 +6,16 @@ type Category = "Todo" | "Doing" | "Done";
 
 type Todo = {
   title: string;
-  isDone: boolean;
   category: Category;
 };
 
 const initialData: Todo[] = [
-  { title: "Finish this app", isDone: false, category: "Todo" },
-  { title: "Writing this app", isDone: false, category: "Doing" },
-  { title: "Research network hardware", isDone: true, category: "Done" },
+  { title: "Finish this app", category: "Todo" },
+  { title: "To cook dinner", category: "Todo" },
+  { title: "Writing this app", category: "Doing" },
+  { title: "Cooking dinner", category: "Doing" },
+  { title: "Planning the masters diploma project", category: "Doing" },
+  { title: "Research network hardware", category: "Done" },
 ];
 
 export default function Page() {
@@ -23,50 +25,50 @@ export default function Page() {
     <div className="min-h-screen bg-sky-50">
       <div className="container mx-auto px-4 py-2">
         <div className="bg-white shadow flex gap-3 p-5">
-          <div className="border-2 border-sky-600 flex-1 p-3 rounded-md">
-            <h2 className="font-bold">Todo</h2>
-            {data
-              .filter((point) => point.category === "Todo")
-              .map((item, index) => (
-                <div className="flex items-center gap-1" key={index}>
-                  <input
-                    type="checkbox"
-                    checked={item.isDone}
-                    readOnly
-                    value={item.title}
-                  />
-                </div>
-              ))}
+          <div className="border border-sky-600 flex-1 p-3 rounded-md">
+            <h2 className="font-bold mb-3">Todo</h2>
+            <div className="gap-3 flex flex-col">
+              {data
+                .filter((point) => point.category === "Todo")
+                .map((item, index) => (
+                  <div
+                    className="border border-sky-600 rounded-md p-3"
+                    key={index}
+                  >
+                    {item.title}
+                  </div>
+                ))}
+            </div>
           </div>
-          <div className="border-2 border-sky-600 flex-1 p-3 rounded-md">
-            <h2 className="font-bold">Doing</h2>
-            {data
-              .filter((point) => point.category === "Doing")
-              .map((item, index) => (
-                <div className="flex items-center gap-1" key={index}>
-                  <input
-                    type="checkbox"
-                    checked={item.isDone}
-                    readOnly
-                    value={item.title}
-                  />
-                </div>
-              ))}
+          <div className="border border-sky-600 flex-1 p-3 rounded-md">
+            <h2 className="font-bold mb-3">Doing</h2>
+            <div className="gap-3 flex flex-col">
+              {data
+                .filter((point) => point.category === "Doing")
+                .map((item, index) => (
+                  <div
+                    className="border border-sky-600 rounded-md p-3"
+                    key={index}
+                  >
+                    {item.title}
+                  </div>
+                ))}
+            </div>
           </div>
-          <div className="border-2 border-sky-600 flex-1 p-3 rounded-md">
-            <h2 className="font-bold">Done</h2>
-            {data
-              .filter((point) => point.category === "Done")
-              .map((item, index) => (
-                <div className="flex items-center gap-1" key={index}>
-                  <input
-                    type="checkbox"
-                    checked={item.isDone}
-                    readOnly
-                    value={item.title}
-                  />
-                </div>
-              ))}
+          <div className="border border-sky-600 flex-1 p-3 rounded-md">
+            <h2 className="font-bold mb-3">Done</h2>
+            <div className="gap-3 flex flex-col">
+              {data
+                .filter((point) => point.category === "Done")
+                .map((item, index) => (
+                  <div
+                    className="border border-sky-600 rounded-md p-3"
+                    key={index}
+                  >
+                    {item.title}
+                  </div>
+                ))}
+            </div>
           </div>
         </div>
       </div>
