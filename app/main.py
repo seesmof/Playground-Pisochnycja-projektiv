d1f5fa9eeacf0a8cbe4f57a9a39bec0d40648fd0
Oleg Onyshchenko <seesmwork@gmail.com>
2026-09-12 20:12:04 +0300

    feat: experiment with pseudo-randomness in Python

--- app/main.py
+++ app/main.py
@@ -1,35 +1,6 @@
-"""
-uvicorn main:app --reload
-"""
+import random
 
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
+# random.seed(36)
 
-app = FastAPI()
-origins = ["*"]
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
-@app.get("/")
-async def index():
-    return {"message": "Jesus is LORD"}
-
-
-class User(BaseModel):
-    name: str
-    age: int
-    email: str
-
-
-@app.post("/users/")
-async def create_user(user: User):
-    return {
-        "user": f"{user.name} is {user.age} years old and has an email {user.email}"
-    }
+print(random.random())
+print(random.random())
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,76 +1,16 @@
-"use client";
-
-import { useState } from "react";
-
-type Category = "Todo" | "Doing" | "Done";
-
-type Todo = {
-  title: string;
-  category: Category;
-};
-
-const initialData: Todo[] = [
-  { title: "Finish this app", category: "Todo" },
-  { title: "To cook dinner", category: "Todo" },
-  { title: "Writing this app", category: "Doing" },
-  { title: "Cooking dinner", category: "Doing" },
-  { title: "Planning the masters diploma project", category: "Doing" },
-  { title: "Research network hardware", category: "Done" },
-];
+import Carousel from "@/components/Carousel";
 
 export default function Page() {
-  const [data, setData] = useState<Todo[]>(initialData);
-
   return (
-    <div className="min-h-screen bg-sky-50">
-      <div className="container mx-auto px-4 py-2">
-        <div className="bg-white shadow flex gap-3 p-5">
-          <div className="border border-sky-600 flex-1 p-3 rounded-md">
-            <h2 className="font-bold mb-3">Todo</h2>
-            <div className="gap-3 flex flex-col">
-              {data
-                .filter((point) => point.category === "Todo")
-                .map((item, index) => (
-                  <div
-                    className="border border-sky-600 rounded-md p-3"
-                    key={index}
-                  >
-                    {item.title}
-                  </div>
-                ))}
-            </div>
-          </div>
-          <div className="border border-sky-600 flex-1 p-3 rounded-md">
-            <h2 className="font-bold mb-3">Doing</h2>
-            <div className="gap-3 flex flex-col">
-              {data
-                .filter((point) => point.category === "Doing")
-                .map((item, index) => (
-                  <div
-                    className="border border-sky-600 rounded-md p-3"
-                    key={index}
-                  >
-                    {item.title}
-                  </div>
-                ))}
-            </div>
-          </div>
-          <div className="border border-sky-600 flex-1 p-3 rounded-md">
-            <h2 className="font-bold mb-3">Done</h2>
-            <div className="gap-3 flex flex-col">
-              {data
-                .filter((point) => point.category === "Done")
-                .map((item, index) => (
-                  <div
-                    className="border border-sky-600 rounded-md p-3"
-                    key={index}
-                  >
-                    {item.title}
-                  </div>
-                ))}
-            </div>
-          </div>
-        </div>
+    <div className="min-h-screen">
+      <div className="max-w-xl mx-auto w-full p-3 md:mt-5">
+        <header className="mb-5">
+          <h1 className="text-3xl md:text-5xl font-black">Carousel</h1>
+          <p className="text-lg text-stone-700 mt-3 md:mt-5">
+            This is a simple app that features a slides carousel.
+          </p>
+        </header>
+        <Carousel />
       </div>
     </div>
   );
