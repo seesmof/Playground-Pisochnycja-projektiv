ad05f9424b8dd58bb35ba004e7cadc853997e197
Oleg Onyshchenko <seesmwork@gmail.com>
2026-09-14 20:31:25 +0300

    chore: copying the code from методичка

--- app/main.py
+++ app/main.py
@@ -1,34 +1,10 @@
-import numpy as np
+import tensorflow as tf
+from tensorflow.keras import datasets, layers, models
 import matplotlib.pyplot as plt
+import numpy as np
 
+(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
+train_images, test_images = train_images / 255.0, test_images / 255.0
 
-def f(x):
-    return x**2
-
-
-def df(x):
-    return 2 * x
-
-
-x = 8.0
-learning_rate = 0.1
-
-history = [x]
-
-for _ in range(20):
-    x = x - learning_rate * df(x)
-    history.append(x)
-
-xs = np.linspace(-10, 10, 400)
-plt.plot(xs, f(xs), label="f(x) = x^2")
-
-history = np.array(history)
-plt.scatter(history, f(history), color="tab:red", zorder=3)
-
-plt.plot(history, f(history), "--", color="tab:blue", alpha=0.7)
-
-plt.xlabel("x")
-plt.ylabel("f(x)")
-plt.legend()
-plt.grid()
-plt.show()
+class_names = "airplane,automobile,bird,cat,deer,dog,frog,horse,ship,truck".split(",")
+print(class_names)
