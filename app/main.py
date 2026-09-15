15371cca2af6eb1a09e88d2822d68ab5d0505642
Oleg Onyshchenko <seesmwork@gmail.com>
2026-09-15 10:08:59 +0300

    chore: trying to copy the code from методичка Amen

--- app/main.py
+++ app/main.py
@@ -8,3 +8,26 @@ train_images, test_images = train_images / 255.0, test_images / 255.0
 
 class_names = "airplane,automobile,bird,cat,deer,dog,frog,horse,ship,truck".split(",")
 print(class_names)
+
+plt.figure(figsize=(10, 10))
+for i in range(25):
+    plt.subplot(5, 5, i + 1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i])
+    plt.xlabel(class_names[train_labels[i][0]])
+plt.show()
+
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation="relu", input_shape=(32, 32, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation="relu"))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation="relu"))
+model.summary()
+
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation="relu"))
+model.add(layers.Dense(10))
+model.summary()
